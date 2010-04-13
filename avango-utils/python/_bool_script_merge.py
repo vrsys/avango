3d004102f94922dbba67206f44941ef3f430ca46
ddangelo <david.d-angelo@iais.fraunhofer.de>
2010-04-13 11:56:34 +0000

    Bug in type check fixed

--- avango-utils/python/_bool_script_merge.py
+++ avango-utils/python/_bool_script_merge.py
@@ -17,6 +17,11 @@ def merge_and_connect_bool_scripts(script1, script2):
         field1_name = script1_field.get_name()
         field1_type = script1_field.get_type()
         
+        #check if the type of the field is not SFBool or
+        #the merged script already does not already contain a field with the given name
+        if "SFBool" not in field1_type or merged_script.get_field(field1_name):
+            continue 
+        
         #get corresponding field of script2
         script2_field = script2.get_field(field1_name)
         field2_type = script1_field.get_type()
@@ -24,11 +29,6 @@ def merge_and_connect_bool_scripts(script1, script2):
         #check if script2 also has a filed with this name and of the same type
         if script2_field == None and field1_type == field2_type:
             continue
-        
-        #check if the type of the field is not SFBool and 
-        #the merged script already does not already contain a field with the given name
-        if field1_type != "SFBool" and merged_script.get_field(field1_name):
-            continue 
         
         new_field = avango._avango._make_field_by_name(script1_field.get_type())
         merged_script.add_field(new_field, field1_name)

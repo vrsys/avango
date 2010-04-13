06bc2489bc34aff885b81bb01041ee2bcf45f8b1
ddangelo <david.d-angelo@iais.fraunhofer.de>
2010-04-13 12:35:07 +0000

    bug fix

--- avango-utils/python/_property_modifier.py
+++ avango-utils/python/_property_modifier.py
@@ -5,12 +5,12 @@ import avango.script
 class PropertyModifierInt(avango.script.Script):
     
     Enable = avango.SFBool()
+    TimeIn = avango.SFDouble()
     
     Property = avango.SFInt()
     PropertyMin = avango.SFInt()
     PropertyMax = avango.SFInt()
     
-    TimeIn = avango.SFDouble()
     
     PropertyStepSize = avango.SFInt()
     PropertyInc = avango.SFBool()
@@ -20,25 +20,28 @@ class PropertyModifierInt(avango.script.Script):
     Triggered = avango.SFBool()
     
     def __init__(self):
-        self.always_evaluate(True)
-        self.last_trigger_time = TimeIn.value
+        self.super(PropertyModifierInt).__init__()
         
-        self.PropertyInc.value = False
-        self.PropertyDec.value = False
-        
+        self.last_trigger_time = self.TimeIn.value
         self.Enable.value = False
         
-        self.PropertyStepSize.value = 1
+        self.Property.value = 0
         self.PropertyMin.value = 0
         self.PropertyMax.value = 100000
+        
+        self.PropertyStepSize.value = 1
+        self.PropertyInc.value = False
+        self.PropertyDec.value = False        
         self.TriggerTimeDelta.value = 0.05
+        
+        self.always_evaluate(True)
         
     def evaluate(self):
         
         if (not self.PropertyInc.value and not self.PropertyDec.value) or not self.Enable.value:
             return
         
-        time = TimeIn.value
+        time = self.TimeIn.value
         time_delta = time - self.last_trigger_time
         if time_delta > self.TriggerTimeDelta.value:
                         

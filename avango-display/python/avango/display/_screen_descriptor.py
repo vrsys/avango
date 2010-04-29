dc18745c9eec19c5c353617b8509d38fd2d6504d
ddangelo <david.d-angelo@iais.fraunhofer.de>
2010-04-29 10:43:08 +0000

    ScreenSpaceToPixelTransform added. This can be used to setup a coordinate system in screenspace with pixel units, opposed to the normalized coordinate system (centered in the origin), which is created by the ScreenSpaceTransform script.

--- avango-display/python/avango/display/_screen_descriptor.py
+++ avango-display/python/avango/display/_screen_descriptor.py
@@ -25,3 +25,29 @@ class ScreenSpaceTransform(avango.script.Script):
         scale = 0.5 * min(self.RealSize.value.x, self.RealSize.value.y)
         scale_mat = avango.osg.make_scale_mat(scale, scale, scale)
         self.Transform.value = scale_mat * self.ScreenTransform.value * self.ViewerTransform.value
+        
+        
+        
+class ScreenSpaceToPixelTransform(avango.script.Script):
+
+    ScreenTransform = avango.osg.SFMatrix()
+    ViewerTransform = avango.osg.SFMatrix()
+    RealSize = avango.osg.SFVec2()
+    PixelSize = avango.osg.SFVec2()
+    Transform = avango.osg.SFMatrix()
+    
+    def __init__(self):
+        self.super(ScreenSpaceToPixelTransform).__init__()
+        self.RealSize.value = avango.osg.Vec2(1,1)
+        self.PixelSize.value = avango.osg.Vec2(1600,1200)
+
+    def evaluate(self):
+        x_trans = -(self.PixelSize.value.x*0.5)
+        y_trans = -(self.PixelSize.value.y*0.5)
+        z_trans = 0
+        trans = avango.osg.Vec3(x_trans,y_trans,z_trans)
+        scale = self.RealSize.value.x / self.PixelSize.value.x
+        scale_mat = avango.osg.make_scale_mat(scale, scale, scale)
+        trans_mat = avango.osg.make_trans_mat(trans)
+
+        self.Transform.value =  trans_mat * scale_mat * self.ScreenTransform.value * self.ViewerTransform.value

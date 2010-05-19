67aa6188a516ea14344cceff2daeb944aaf62527
ddangelo <david.d-angelo@iais.fraunhofer.de>
2010-05-19 14:49:41 +0000

    Samples converted to use avango.display.

--- examples/inspector/inspector.py
+++ examples/inspector/inspector.py
@@ -4,7 +4,7 @@
 #                                                                        #
 # This file is part of AVANGO.                                           #
 #                                                                        #
-# Copyright 1997 - 2009 Fraunhofer-Gesellschaft zur Foerderung der       #
+# Copyright 1997 - 2010 Fraunhofer-Gesellschaft zur Foerderung der       #
 # angewandten Forschung (FhG), Munich, Germany.                          #
 #                                                                        #
 # AVANGO is free software: you can redistribute it and/or modify         #
@@ -23,9 +23,14 @@
 
 import avango.osg
 import avango.inspector
-import avango.osg.simpleviewer
+import avango.display
 
-root = avango.osg.nodes.Group(Name="Root")
+import sys
+
+argv = avango.display.init(sys.argv)
+view = avango.display.make_view()
+
+root = avango.osg.nodes.MatrixTransform(Name="Root",Matrix=avango.osg.make_trans_mat(0,1.7,-20))
 sphere1 = avango.osg.nodes.Sphere(Name="RedSphere", Color=avango.osg.Vec4(1., 0., 0., 1))
 sphere1.Matrix.value = avango.osg.make_trans_mat(1, 0, 0)
 sphere2 = avango.osg.nodes.Sphere(Name="GreenSphere", Color=avango.osg.Vec4(0., 1., 0., 1))
@@ -33,4 +38,7 @@ sphere2.Matrix.value = avango.osg.make_trans_mat(-1, 0, 0)
 root.Children.value = [ sphere1, sphere2 ]
 
 inspector = avango.inspector.nodes.Inspector(Children=[root])
-avango.osg.simpleviewer.run(root)
+
+view.Root.value = root
+
+avango.display.run()
--- examples/inspector/wobble.py
+++ examples/inspector/wobble.py
@@ -23,28 +23,38 @@
 
 import avango.osg
 import avango.inspector
-import avango.osg.simpleviewer
+import avango.display
+
 import math
+import sys
+
+argv = avango.display.init(sys.argv)
+view = avango.display.make_view()
 
 time_sensor = avango.nodes.TimeSensor()
 
-root = avango.osg.nodes.Group(Name="Root")
+root = avango.osg.nodes.MatrixTransform(Name="Root",Matrix=avango.osg.make_trans_mat(0,1.7,-20))
 sphere1 = avango.osg.nodes.Sphere(Name="RedSphere", Color=avango.osg.Vec4(1., 0., 0., 1), DetailRatio = 0.8)
 sphere1.Matrix.value = avango.osg.make_trans_mat(1, 0, 0)
 sphere2 = avango.osg.nodes.Sphere(Name="GreenSphere", Color=avango.osg.Vec4(0., 1., 0., 1), DetailRatio = 0.8)
 sphere2.Matrix.value = avango.osg.make_trans_mat(-1, 0, 0)
 root.Children.value = [ sphere1, sphere2 ]
 
+
 class Wobble(avango.script.Script):
     TimeIn = avango.SFFloat()
     MatrixOut = avango.osg.SFMatrix()
+    Offset = avango.SFFloat()
 
     def evaluate(self):
-        self.MatrixOut.value = avango.osg.make_trans_mat(math.sin(self.TimeIn.value), 0., 0.)
+        self.MatrixOut.value = avango.osg.make_trans_mat( self.Offset.value + math.sin(self.TimeIn.value), 0., 0.)
 
 wobble = Wobble(Name='Wobble')
 sphere1.Matrix.connect_from(wobble.MatrixOut)
 wobble.TimeIn.connect_from(time_sensor.Time)
 
 inspector = avango.inspector.nodes.Inspector(Children=[root])
-avango.osg.simpleviewer.run(root)
+
+view.Root.value = root
+
+avango.display.run()

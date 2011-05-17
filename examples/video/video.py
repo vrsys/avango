97fdc93921752762f2d0d59c0742a781bd01e6c5
mschlatt <mschlatt@iais.fraunhofer.de>
2011-05-17 12:54:07 +0000

    video

--- examples/video/video.py
+++ examples/video/video.py
@@ -29,7 +29,24 @@ import avango.osg.viewer
 import avango.moving
 import sys
 import math
+import avango
+import avango.display
+import sys
 
+
+argv = avango.display.init(sys.argv)
+view = avango.display.make_view()
+view.EnableTrackball.value = True
+
+#root_obj = avango.osg.nodes.MatrixTransform(Matrix=avango.osg.make_trans_mat(0,1.7,-1.2))
+#view.Root.value = root_obj
+view.Root.value = avango.osg.nodes.Group()
+
+sphere = avango.osg.nodes.Sphere(Radius=0.1)
+
+#avango.display.run()
+
+"""
 # set up scene graph
 root_group = avango.osg.nodes.Group()
 
@@ -49,30 +66,47 @@ events = avango.osg.viewer.nodes.EventFields(View = viewer)
 window.ToggleFullScreen.connect_from(events.KeyAltReturn)
 window.DragEvent.connect_from(events.DragEvent)
 window.MoveEvent.connect_from(events.MoveEvent)
+"""
 
 # load a video
 loadimagestream = avango.osg.nodes.LoadImageStream(
                     Filename = sys.argv[1])
-viewer.frame()
+#viewer.frame()
+avango.evaluate()
 
 video = loadimagestream.ImageStream.get_value()
 video.Loop.value = True
 video.Status.value=avango.osg.streamstatus.playing
 
 videoTexture = avango.osg.nodes.Texture2D(Image = video, ResizeNonPowerOfTwoHint = False)
-ss = avango.osg.nodes.StateSet(Texture0 = videoTexture)
-obj = avango.osg.nodes.Sphere(Name="TexturedSphere", StateSet = ss)
+#s = avango.osg.nodes.StateSet(Texture0 = videoTexture)
 
-root_group.Children.value = [obj]
+
+print videoTexture.Image.value.Width.value
+print videoTexture.TextureHeight.value
+
+
+quad = avango.osg.nodes.TexturedQuad()
+quad.Texture2D.value = videoTexture
+quad.UseFilename.value = False
+
+geode = avango.osg.nodes.Geode(Drawables = [quad])
+
+#obj = avango.osg.nodes.Sphere(Name="TexturedSphere", StateSet = ss)
+
+view.Root.value.Children.value.append(geode)
+#view.Root.value.Children.value.append(sphere)
+#root_group.Children.value = [quad]
 
 # render a frame to update bounding spheres and scale model to fit in window
-viewer.frame()
-scale = 0.08 / obj.get_bounding_sphere().radius()
+#viewer.frame()
+#scale = 0.08 / obj.get_bounding_sphere().radius()
 
-obj.Matrix.value = \
-  avango.osg.make_rot_mat(math.radians(90), 1,0,0) * \
-  avango.osg.make_rot_mat(math.radians(90), 0,1,0) * \
-  avango.osg.make_trans_mat(0,0,-5) * \
-  avango.osg.make_scale_mat(scale,scale,scale)
+#obj.Matrix.value = \
+#  avango.osg.make_rot_mat(math.radians(90), 1,0,0) * \
+#  avango.osg.make_rot_mat(math.radians(90), 0,1,0) * \
+#  avango.osg.make_trans_mat(0,0,-5) * \
+#  avango.osg.make_scale_mat(scale,scale,scale)
 
-viewer.run()
+#viewer.run()
+avango.display.run()

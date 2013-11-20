a1ccb7aff178b3baf0c016acdf8d316e5c779886
Simon Schneegans <code@simonschneegans.de>
2013-11-20 15:04:13 +0100

    added picking example and tutorial and made start scripts cope with white spaces

--- examples/physics_example/start.sh
+++ examples/physics_example/start.sh
@@ -4,8 +4,8 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 # assuming a local guacmole version is located properly
-LOCAL_GUACAMOLE=$DIR/../../../guacamole
-LOCAL_AVANGO=$DIR/../../../avango
+LOCAL_GUACAMOLE="$DIR/../../../guacamole"
+LOCAL_AVANGO="$DIR/../../../avango"
 
 # if not, this path will be used
 GUACAMOLE=/opt/guacamole/current/guacamole
@@ -18,22 +18,22 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/boost/current/lib:/opt/openscenegra
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/schism/current/lib/linux_x86
 
 # avango
-export LD_LIBRARY_PATH=$LOCAL_AVANGO/lib:$AVANGO/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=$LOCAL_AVANGO/lib:$LOCAL_AVANGO/examples:$AVANGO/lib:$AVANGO/examples:$PYTHONPATH
+export LD_LIBRARY_PATH="$LOCAL_AVANGO/lib":$AVANGO/lib:$LD_LIBRARY_PATH
+export PYTHONPATH="$LOCAL_AVANGO/lib":"$LOCAL_AVANGO/examples":$AVANGO/lib:$AVANGO/examples:$PYTHONPATH
 
 # guacamole
-export LD_LIBRARY_PATH=$LOCAL_GUACAMOLE/lib:$GUACAMOLE/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="$LOCAL_GUACAMOLE/lib":$GUACAMOLE/lib:$LD_LIBRARY_PATH
 
 # run daemon
-if [ -f $LOCAL_AVANGO/examples/examples_common/daemon.py ]
+if [ -f "$LOCAL_AVANGO/examples/examples_common/daemon.py" ]
 then
-    $LOCAL_AVANGO/examples/examples_common/daemon.py > /dev/null &
+    "$LOCAL_AVANGO/examples/examples_common/daemon.py" > /dev/null &
 else
-    $AVANGO/examples/examples_common/daemon.py > /dev/null &
+    "$AVANGO/examples/examples_common/daemon.py" > /dev/null &
 fi
 
 # run program
-cd $DIR && ./main.py
+cd "$DIR" && ./main.py
 
 # kill daemon
 kill %1

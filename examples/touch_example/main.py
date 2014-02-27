95c64cf4642c89bf672ad7c59df4a736133d1403
Simon Schneegans <code@simonschneegans.de>
2014-02-27 14:49:34 +0100

    worked on touch example

--- examples/touch_example/main.py
+++ examples/touch_example/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import math
 import avango
 import avango.script
 from avango.script import field_has_changed
@@ -10,18 +11,53 @@ from examples_common.GuaVE import GuaVE
 
 width = 1920
 size = avango.gua.Vec2ui(width, width * 9 /16)
+timer = avango.nodes.TimeSensor()
 
 CUBE_COUNT_X = 10
 CUBE_COUNT_Y = 3
 CUBE_COUNT_Z = 1
 
-class TimedRotate(avango.script.Script):
+class CameraMover(avango.script.Script):
   TimeIn = avango.SFFloat()
   MatrixOut = avango.gua.SFMatrix4()
 
+  MatrixStart = avango.gua.SFMatrix4()
+
   @field_has_changed(TimeIn)
   def update(self):
-    self.MatrixOut.value = avango.gua.make_rot_mat(self.TimeIn.value*2.0, 0.0, 1.0, 0.0)
+    self.MatrixOut.value = self.MatrixStart.value * \
+      avango.gua.make_rot_mat(math.sin(self.TimeIn.value*0.5)*2, 0, 1, 0) * \
+      avango.gua.make_trans_mat(math.sin(self.TimeIn.value*0.5)*2.0, 0.0, math.cos(self.TimeIn.value*0.5)*2.0 - 2.0)
+
+
+def add_camera(graph, parent):
+  eye = avango.gua.nodes.TransformNode(
+    Name = "eye",
+    Transform = avango.gua.make_trans_mat(0.0, 0.0, 4)
+  )
+
+  screen = avango.gua.nodes.ScreenNode(
+    Name = "screen",
+    Width = 8,
+    Height = 4.5,
+    Children = [eye]
+  )
+
+  camera_mover = CameraMover(
+    MatrixStart = avango.gua.make_trans_mat(0, 0, 3)
+  )
+  camera_mover.TimeIn.connect_from(timer.Time)
+  screen.Transform.connect_from(camera_mover.MatrixOut)
+
+  parent.Children.value.append(screen)
+
+  camera = avango.gua.nodes.Camera(
+    LeftEye = eye.Path.value,
+    LeftScreen = screen.Path.value,
+    SceneGraph = graph.Name.value
+  )
+
+  return camera
 
 def create_pipeline():
   return avango.gua.nodes.Pipeline(
@@ -30,6 +66,35 @@ def create_pipeline():
     BackgroundMode = avango.gua.BackgroundMode.SKYMAP_TEXTURE,
     AmbientColor = avango.gua.Color(1, 1, 1)
   )
+
+def add_split_screen(dynamic_split_screens, location):
+
+  current_split_locations = []
+
+  for split in dynamic_split_screens.SplitScreens:
+    current_split_locations.append(split.Location.value)
+
+  closest = get_closest(location, current_split_locations)
+  closest_camera = dynamic_split_screens.SplitScreens[closest].Pipe.value.Camera.value
+
+  new_split = dynamic_split_screens.add_split_screen(closest_camera, location)
+
+  camera_transform = avango.gua.nodes.TransformNode(
+    Name = "camera_transform" + str(new_split.ID.value),
+    Transform = dynamic_split_screens.GraphIn.value[closest_camera.LeftScreen.value].Transform.value
+  )
+  dynamic_split_screens.GraphIn.value.Root.value.Children.value.append(camera_transform)
+
+  camera = add_camera(dynamic_split_screens.GraphIn.value, camera_transform)
+  new_split.Pipe.value.Camera.value = camera
+
+  camera_mover = CameraMover(
+    MatrixStart = camera_transform.Transform.value
+  )
+  camera_mover.TimeIn.connect_from(timer.Time)
+  dynamic_split_screens.GraphIn.value[camera.LeftScreen.value].Transform.connect_from(camera_mover.MatrixOut)
+
+  return new_split
 
 def start():
 
@@ -47,12 +112,9 @@ def start():
   graph.Root.value.Children.value.append(light)
 
   loader = avango.gua.nodes.GeometryLoader()
-  # timer = avango.nodes.TimeSensor()
-
   for x in range(0, CUBE_COUNT_X):
     for y in range(0, CUBE_COUNT_Y):
       for z in range(0, CUBE_COUNT_Z):
-
         new_cube = loader.create_geometry_from_file("cube" + str(x) + str(y) + str(z),
                   "data/objects/monkey.obj",
                   "White",
@@ -62,30 +124,8 @@ def start():
                                    avango.gua.make_scale_mat(0.3, 0.3, 0.3)
         graph.Root.value.Children.value.append(new_cube)
 
-  # monkey_updater = TimedRotate()
-  # monkey_updater.TimeIn.connect_from(timer.Time)
-  # monkey.Transform.connect_from(monkey_updater.MatrixOut)
 
-  eye = avango.gua.nodes.TransformNode(
-    Name = "eye",
-    Transform = avango.gua.make_trans_mat(0.0, 0.0, 4)
-  )
-
-  screen = avango.gua.nodes.ScreenNode(
-    Name = "screen",
-    Width = 8,
-    Height = 4.5,
-    Transform = avango.gua.make_trans_mat(0, 0, 3),
-    Children = [eye]
-  )
-
-  graph.Root.value.Children.value.append(screen)
-
-  camera = avango.gua.nodes.Camera(
-    LeftEye = "/screen/eye",
-    LeftScreen = "/screen",
-    SceneGraph = graph.Name.value
-  )
+  camera = add_camera(graph, graph.Root.value)
 
   split_screens = DynamicSplitScreens(
     GraphIn = graph,
@@ -93,10 +133,9 @@ def start():
   )
 
   split_screens.init(5, create_pipeline)
+  root_split_screen = split_screens.add_split_screen(camera, avango.gua.Vec2(0, 0))
 
-  s = split_screens.add_split_screen(camera, avango.gua.Vec2(0, 0))
-  r = split_screens.add_split_screen(camera, avango.gua.Vec2(0.5, 0.5))
-  t = split_screens.add_split_screen(camera, avango.gua.Vec2(-0.5, -0.5))
+  #add_split_screen(split_screens, avango.gua.Vec2(0.3, 0.3))
 
   #setup viewer
   viewer = avango.gua.nodes.Viewer()

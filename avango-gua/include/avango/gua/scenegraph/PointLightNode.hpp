86b42adfcb94f242652c84144274cfa5b7f0ffbe
Andreas-Christoph Bernstein <andreas.bernstein@uni-weimar.de>
2014-12-10 16:07:26 +0100

    Add brightness field.

--- avango-gua/include/avango/gua/scenegraph/PointLightNode.hpp
+++ avango-gua/include/avango/gua/scenegraph/PointLightNode.hpp
@@ -42,6 +42,7 @@ namespace av
     public:
 
       SFColor Color;
+      SFFloat Brightness;
       SFFloat Falloff;
       SFBool EnableShadows;
       SFBool EnableGodrays;
@@ -58,6 +59,9 @@ namespace av
 
       virtual void getColorCB(const SFColor::GetValueEvent& event);
       virtual void setColorCB(const SFColor::SetValueEvent& event);
+
+      virtual void getBrightnessCB(const SFFloat::GetValueEvent& event);
+      virtual void setBrightnessCB(const SFFloat::SetValueEvent& event);
 
       virtual void getFallofCB(const SFFloat::GetValueEvent& event);
       virtual void setFallofCB(const SFFloat::SetValueEvent& event);

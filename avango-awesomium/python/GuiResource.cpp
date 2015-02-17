8b0e4d7d8d0a3635d9854b7f1725f679edcbc29c
Andreas-Christoph Bernstein <andreas.bernstein@uni-weimar.de>
2015-02-17 13:14:51 +0100

    Add avango-awesomium module.

--- avango-awesomium/python/GuiResource.cpp
+++ avango-awesomium/python/GuiResource.cpp
@@ -18,13 +18,13 @@ namespace boost
   }
 }
 
-void register_on_loaded(av::gua::GuiResource& gui, boost::python::object& func) {
+void register_on_loaded(av::gua::gui::GuiResource& gui, boost::python::object& func) {
   gui.on_loaded([func]() {
     func();
   });
 }
 
-void call_javascript(av::gua::GuiResource& gui, std::string const& method, list const& args) {
+void call_javascript(av::gua::gui::GuiResource& gui, std::string const& method, list const& args) {
     std::vector<std::string> args_vec;
 
     for (int i(0); i < len(args); ++i) {
@@ -34,7 +34,7 @@ void call_javascript(av::gua::GuiResource& gui, std::string const& method, list 
     gui.call_javascript(method, args_vec);
 }
 
-void register_on_javascript_callback(av::gua::GuiResource& gui, boost::python::object& func) {
+void register_on_javascript_callback(av::gua::gui::GuiResource& gui, boost::python::object& func) {
   gui.on_javascript_callback([func](std::string const& method, std::vector<std::string> const& args) {
     list args_list;
     for (auto const& arg : args){
@@ -44,7 +44,7 @@ void register_on_javascript_callback(av::gua::GuiResource& gui, boost::python::o
   });
 }
 
-void add_javascript_getter(av::gua::GuiResource& gui, std::string const& name, boost::python::object& func) {
+void add_javascript_getter(av::gua::gui::GuiResource& gui, std::string const& name, boost::python::object& func) {
   auto getter = [func]() -> std::string {
     return extract<std::string>(func());
   };
@@ -54,27 +54,27 @@ void add_javascript_getter(av::gua::GuiResource& gui, std::string const& name, b
 
 void init_GuiResource()
 {
-  register_field<av::gua::SFGuiResource>("SFGuiResource");
-  register_multifield<av::gua::MFGuiResource>("MFGuiResource");
-  class_<av::gua::GuiResource, av::Link<av::gua::GuiResource>,
+  register_field<av::gua::gui::SFGuiResource>("SFGuiResource");
+  register_multifield<av::gua::gui::MFGuiResource>("MFGuiResource");
+  class_<av::gua::gui::GuiResource, av::Link<av::gua::gui::GuiResource>,
     bases<av::FieldContainer>,
     boost::noncopyable >("GuiResource", "docstring", no_init)
-    .def("init", &av::gua::GuiResource::init)
-    .def("go_forward", &av::gua::GuiResource::go_forward)
-    .def("go_back", &av::gua::GuiResource::go_back)
-    .def("go_to_history_offset", &av::gua::GuiResource::go_to_history_offset)
-    .def("reload", &av::gua::GuiResource::reload)
-    .def("focus", &av::gua::GuiResource::focus)
-    .def("inject_keyboard_event", &av::gua::GuiResource::inject_keyboard_event)
-    .def("inject_char_event", &av::gua::GuiResource::inject_char_event)
-    .def("inject_mouse_position_relative", &av::gua::GuiResource::inject_mouse_position_relative)
-    .def("inject_mouse_position", &av::gua::GuiResource::inject_mouse_position)
-    .def("inject_mouse_button", &av::gua::GuiResource::inject_mouse_button)
-    .def("inject_mouse_wheel", &av::gua::GuiResource::inject_mouse_wheel)
+    .def("init", &av::gua::gui::GuiResource::init)
+    .def("go_forward", &av::gua::gui::GuiResource::go_forward)
+    .def("go_back", &av::gua::gui::GuiResource::go_back)
+    .def("go_to_history_offset", &av::gua::gui::GuiResource::go_to_history_offset)
+    .def("reload", &av::gua::gui::GuiResource::reload)
+    .def("focus", &av::gua::gui::GuiResource::focus)
+    .def("inject_keyboard_event", &av::gua::gui::GuiResource::inject_keyboard_event)
+    .def("inject_char_event", &av::gua::gui::GuiResource::inject_char_event)
+    .def("inject_mouse_position_relative", &av::gua::gui::GuiResource::inject_mouse_position_relative)
+    .def("inject_mouse_position", &av::gua::gui::GuiResource::inject_mouse_position)
+    .def("inject_mouse_button", &av::gua::gui::GuiResource::inject_mouse_button)
+    .def("inject_mouse_wheel", &av::gua::gui::GuiResource::inject_mouse_wheel)
     .def("on_loaded", register_on_loaded)
     .def("call_javascript", call_javascript)
     .def("on_javascript_callback", register_on_javascript_callback)
-    .def("add_javascript_callback", &av::gua::GuiResource::add_javascript_callback)
+    .def("add_javascript_callback", &av::gua::gui::GuiResource::add_javascript_callback)
     .def("add_javascript_getter", add_javascript_getter)
     ;
 }

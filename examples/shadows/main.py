e5d28a7bca685f68cf20d08334c0aee299e36675
Simon Schneegans <code@simonschneegans.de>
2015-03-05 13:56:49 +0100

    ported shadow example to new guacamole

--- examples/shadows/main.py
+++ examples/shadows/main.py
@@ -6,67 +6,9 @@ import avango.gua
 import examples_common.navigator
 from examples_common.GuaVE import GuaVE
 
-CUBE_COUNT_X = 30
-CUBE_COUNT_Y = 3
-CUBE_COUNT_Z = 30
-
-def setup_pipe():
-  camera = avango.gua.nodes.Camera(LeftEye = "/eye",
-                                   RightEye = "/eye",
-                                   LeftScreen = "/eye/screen",
-                                   RightScreen = "/eye/screen",
-                                   SceneGraph = "scene")
-
-  width = 1920;
-  height = int(width * 9.0 / 16.0)
-  size = avango.gua.Vec2ui(width, height)
-
-  window = avango.gua.nodes.Window(Size = size,
-                                   Title = "weimar",
-                                   LeftResolution = size)
-
-  pipe = avango.gua.nodes.Pipeline(Camera = camera,
-                                   Window = window,
-                                   LeftResolution = size)
-
-
-  avango.gua.create_texture("data/textures/skymap.jpg")
-
-  pipe.OutputTextureName.value = "weimar_pipe"
-  pipe.EnableSsao.value = True
-  pipe.SsaoRadius.value = 5
-  pipe.SsaoIntensity.value = 0.7
-
-
-  pipe.EnableFog.value = True
-  pipe.FogTexture.value = "data/textures/skymap.jpg"
-  pipe.FogStart.value = 20.0
-  pipe.FogEnd.value = 50.0
-
-  pipe.BackgroundTexture.value = "data/textures/skymap.jpg"
-
-  pipe.EnableVignette.value = True
-  pipe.VignetteColor.value = avango.gua.Color()
-  pipe.VignetteCoverage.value = 0.5
-  pipe.VignetteSoftness.value = 0.6
-  pipe.EnablePreviewDisplay.value = False
-
-  pipe.NearClip.value = 0.1
-  pipe.FarClip.value = 1000
-
-  pipe.EnableFXAA.value = True
-  pipe.EnableFrustumCulling.value = True
-
-  pipe.EnableFPSDisplay.value = True
-  pipe.EnableRayDisplay.value = True
-
-  pipe.EnableBloom.value = False
-  pipe.EnableHDR.value = False
-  pipe.HDRKey.value = 0.1
-
-  pipe.BackgroundMode.value = avango.gua.BackgroundMode.SKYMAP_TEXTURE
-
-  return pipe
+CUBE_COUNT_X = 20
+CUBE_COUNT_Y = 2
+CUBE_COUNT_Z = 20
 
 def start():
 
@@ -77,10 +19,11 @@ def start():
     for y in range(0, CUBE_COUNT_Y):
       for z in range(0, CUBE_COUNT_Z):
 
-        new_cube = loader.create_geometry_from_file("cube" + str(x) + str(y) + str(z),
-                  "data/objects/monkey.obj",
-                  "data/materials/White.gmd",
-                  avango.gua.LoaderFlags.DEFAULTS)
+        new_cube = loader.create_geometry_from_file(
+          "cube" + str(x) + str(y) + str(z),
+          "data/objects/monkey.obj",
+          avango.gua.LoaderFlags.DEFAULTS
+        )
 
         new_cube.Transform.value = avango.gua.make_trans_mat(x*2, y*2, z*2) * \
                                    avango.gua.make_scale_mat(0.3, 0.3, 0.3)
@@ -93,7 +36,8 @@ def start():
                                          ShadowMapSize = 1024,
                                          ShadowOffset = 0.001,
                                          ShadowCascadedSplits = [0.1, 3, 8, 15, 40],
-                                         ShadowNearClippingInSunDirection = 100
+                                         ShadowNearClippingInSunDirection = 100,
+                                         Brightness = 2
                                          )
   sun_light.Transform.value = avango.gua.make_rot_mat(210, 0, 1, 0) * avango.gua.make_rot_mat(-30.0, 1.0, 1.0, 0.0)
   graph.Root.value.Children.value.append(sun_light)
@@ -103,15 +47,27 @@ def start():
                                          EnableGodrays = True,
                                          EnableShadows = True,
                                          ShadowMapSize = 256,
-                                         ShadowOffset = 0.0005)
-  spot_light.Transform.value = avango.gua.make_trans_mat(8.0, 3.0, 8.0) * avango.gua.make_rot_mat(-90, 1, 0, 0) * avango.gua.make_scale_mat(10, 10, 10)
+                                         ShadowOffset = 0.0005,
+                                         Falloff = 1.5,
+                                         Softness = 2,
+                                         Brightness = 10)
+  spot_light.Transform.value = avango.gua.make_trans_mat(14.0, 3.0, 18.0) * avango.gua.make_rot_mat(-20, 1, 0, 0) * avango.gua.make_scale_mat(30)
   graph.Root.value.Children.value.append(spot_light)
+
+
+  point_light = avango.gua.nodes.PointLightNode(Name = "point_light",
+                                         Color = avango.gua.Color(0.2, 1.0, 0.7),
+                                         EnableGodrays = True,
+                                         Brightness = 10)
+  point_light.Transform.value = avango.gua.make_trans_mat(4.0, 1.0, 4.0) * avango.gua.make_scale_mat(10)
+  graph.Root.value.Children.value.append(point_light)
 
 
 
   top_light = avango.gua.nodes.SunLightNode(Name = "top_light",
                                          Color = avango.gua.Color(0.1, 0.2, 0.4),
-                                         EnableSpecularShading = False)
+                                         EnableSpecularShading = False,
+                                         Brightness = 1)
   top_light.Transform.value = avango.gua.make_rot_mat(-90.0, 1.0, 0.0, 0.0)
   graph.Root.value.Children.value.append(top_light)
 
@@ -119,44 +75,65 @@ def start():
 
   fill_light = avango.gua.nodes.SunLightNode(Name = "fill_light",
                                          Color = avango.gua.Color(0.05, 0.1, 0.05),
-                                         EnableSpecularShading = False)
+                                         EnableSpecularShading = False,
+                                         Brightness = 1)
   fill_light.Transform.value = avango.gua.make_rot_mat(90.0, 1.0, 0.0, 0.0)
   graph.Root.value.Children.value.append(fill_light)
 
-  floor = loader.create_geometry_from_file("floor",
-                  "data/objects/plane.obj",
-                  "data/materials/White.gmd",
-                  avango.gua.LoaderFlags.DEFAULTS)
-  floor.Transform.value = avango.gua.make_scale_mat(30, 1, 30) * avango.gua.make_trans_mat(1, -0.2, 1)
+
+  floor = loader.create_geometry_from_file(
+    "floor",
+    "data/objects/plane.obj",
+    avango.gua.LoaderFlags.DEFAULTS
+  )
+  floor.Transform.value = avango.gua.make_scale_mat(20, 1, 20) * avango.gua.make_trans_mat(1, -0.2, 1)
   graph.Root.value.Children.value.append(floor)
+
+  width = 1920;
+  height = int(width * 9.0 / 16.0)
+  size = avango.gua.Vec2ui(width, height)
 
   screen = avango.gua.nodes.ScreenNode(Name = "screen",
                                        Width = 4.8,
                                        Height = 2.7)
 
-
   screen.Transform.value = avango.gua.make_trans_mat(0.0, 0.0, -2.5)
 
-  eye = avango.gua.nodes.TransformNode(Name = "eye")
-  eye.Transform.value = avango.gua.make_trans_mat(0.0, 1.35, 20.0)
-  eye.Children.value = [screen]
+  camera = avango.gua.nodes.CameraNode(
+    Name = "cam",
+    LeftScreenPath = "/cam/screen",
+    SceneGraph = "scene",
+    Resolution = size,
+    OutputWindowName = "window",
+    Children = [screen],
+    Transform = avango.gua.make_trans_mat(0.0, 0.0, 7.0)
+  )
 
-  graph.Root.value.Children.value.append(eye)
+  camera.PipelineDescription.value.EnableABuffer.value = True
 
-  pipe = setup_pipe()
+  graph.Root.value.Children.value.append(camera)
+
+  window = avango.gua.nodes.Window(
+    Size = size,
+    Title = "shadows",
+    LeftResolution = size
+  )
+
+  avango.gua.register_window("window", window)
 
   navigator = examples_common.navigator.Navigator()
-  navigator.StartLocation.value = eye.Transform.value.get_translate()
-  navigator.OutTransform.connect_from(eye.Transform)
+  navigator.StartLocation.value = camera.Transform.value.get_translate()
+  navigator.OutTransform.connect_from(camera.Transform)
 
   navigator.RotationSpeed.value = 0.2
   navigator.MotionSpeed.value = 0.04
 
-  eye.Transform.connect_from(navigator.OutTransform)
+  camera.Transform.connect_from(navigator.OutTransform)
 
   viewer = avango.gua.nodes.Viewer()
-  viewer.Pipelines.value = [pipe]
+  viewer.CameraNodes.value = [camera]
   viewer.SceneGraphs.value = [graph]
+  viewer.Windows.value = [window]
 
   guaVE = GuaVE()
   guaVE.start(locals(), globals())

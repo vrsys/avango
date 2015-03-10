be856184cca24df02a909a3cd4183e0aace584f6
Sebastian Utzig <sebastian.utzig@uni-weimar.de>
2015-03-10 19:23:58 +0100

    USE dynamic screen scale in CameraControl

--- examples/skeletal_animation/CameraControl.py
+++ examples/skeletal_animation/CameraControl.py
@@ -37,8 +37,9 @@ class CameraControl(avango.script.Script):
     # camera steering:
     if self._mouse_pos != None and self._mouse_scroll != None:
       target_trans = self._target.WorldTransform.value.get_translate()
-      self._screen.Transform.value = avango.gua.make_trans_mat(target_trans+avango.gua.Vec3(0.0,0.04,0.0)) * \
+      target_scale = self._target.WorldTransform.value.get_scale()
+      self._screen.Transform.value = avango.gua.make_trans_mat(target_trans+avango.gua.Vec3(0.0,0.1,0.0)) * \
         avango.gua.make_rot_mat(self._mouse_pos.x/2.0,0.0,1.0,0.0) * \
         avango.gua.make_rot_mat(self._mouse_pos.y/2.0,1.0,0.0,0.0) * \
         avango.gua.make_trans_mat(0,0.003*self._mouse_scroll.y,0.05*self._mouse_scroll.y) * \
-        avango.gua.make_scale_mat(0.04,0.04,0.04)# * avango.gua.make_rot_mat(180.0,0.0,1.0,0.0)
+        avango.gua.make_scale_mat(target_scale)# * avango.gua.make_rot_mat(180.0,0.0,1.0,0.0)

1a217ed731ebbc62211bc2b9f3641aa51afc0ae3
Sebastian Utzig <sebastian.utzig@uni-weimar.de>
2015-03-10 19:25:48 +0100

    WIP improved working GroundFollowing

--- examples/skeletal_animation/main.py
+++ examples/skeletal_animation/main.py
@@ -36,52 +36,63 @@ class GroundFollowing(avango.script.Script):
     global cubes
 
     in_translation = self.InTransform.value.get_translate()
-
-    in_translation.y += (self.OffsetToGround.value * 2.0)+0.03 #0.03 as animation offset
+    in_translation.y += self.OffsetToGround.value
 
     self.__ray.Transform.value = avango.gua.make_trans_mat(in_translation) *\
                                  avango.gua.make_rot_mat(-90, 1, 0, 0) *\
-                                 avango.gua.make_scale_mat(1.0, 1.0, self.MaxDistanceToGround.value + self.OffsetToGround.value * 2.0)
+                                 avango.gua.make_scale_mat(1.0, 1.0, self.MaxDistanceToGround.value)
 
     results = self.SceneGraph.value.ray_test(
                                      self.__ray,
                                      avango.gua.PickingOptions.PICK_ONLY_FIRST_OBJECT |
-                                     #avango.gua.PickingOptions.GET_POSITIONS)
-                                     avango.gua.PickingOptions.GET_WORLD_POSITIONS)
+                                     avango.gua.PickingOptions.GET_POSITIONS)
 
-    #result = self.InTransform.value
-    result = avango.gua.Vec3(0.0,0.0,0.0)
+    
+    #intersection test geometry:
+    '''tri_mesh_loader = avango.gua.nodes.TriMeshLoader()
+    cube1 = tri_mesh_loader.create_geometry_from_file("cube",
+                                        "data/objects/cone.obj",
+                                        avango.gua.LoaderFlags.NORMALIZE_POSITION
+                                        | avango.gua.LoaderFlags.NORMALIZE_SCALE)
+    cube = tri_mesh_loader.create_geometry_from_file("cube",
+                                        "data/objects/cone.obj",
+                                        avango.gua.LoaderFlags.NORMALIZE_POSITION
+                                        | avango.gua.LoaderFlags.NORMALIZE_SCALE)
+    #cube.Transform.value = avango.gua.make_trans_mat(first_hit.WorldPosition.value) * avango.gua.make_scale_mat(0.01,0.01,0.01)
+    #cube1.Transform.value = avango.gua.make_trans_mat(hit_world_trans) * avango.gua.make_scale_mat(0.01,1.0,0.01)
+    #display ray of ray test:
+    cube.Transform.value = avango.gua.make_trans_mat(in_translation) *\
+                             avango.gua.make_rot_mat(-90, 1, 0, 0) *\
+                             avango.gua.make_scale_mat(0.01, 0.01, self.MaxDistanceToGround.value * 0.5)
+    if self.SceneGraph.value != None:
+      self.SceneGraph.value.Root.value.Children.value.append(cube)
+      #self.SceneGraph.value.Root.value.Children.value.append(cube1)
+      cubes.append(cube)
+      #cubes.append(cube1)
+    while len(cubes)>300:
+      tmp_cube = cubes.pop(0)
+      self.SceneGraph.value.Root.value.Children.value.remove(tmp_cube)'''
+
+
+    delta_trans = avango.gua.Vec3(0.0,0.0,0.0)
 
     if len(results.value) > 0:
 
       first_hit = results.value[0]
 
-      #hit_world = avango.gua.make_trans_mat(first_hit.WorldPosition.value) * first_hit.Object.value.Transform.value
+      hit_world = first_hit.Object.value.Transform.value * avango.gua.make_trans_mat(first_hit.Position.value)
+      
+      hit_world_trans = hit_world.get_translate()
+      #hit_world_trans = first_hit.WorldPosition.value
+
+      new_pos = avango.gua.make_trans_mat(hit_world_trans)# * avango.gua.make_trans_mat(0.0,self.OffsetToGround.value, 0.0)
+
       if light2 != None:
-        light2.Transform.value = avango.gua.make_trans_mat(first_hit.WorldPosition.value)
-        #light2.Transform.value = hit_world
+        light2.Transform.value = new_pos * avango.gua.make_trans_mat(0.0,0.2,0.0)
 
-        #intersection test geometry:
-        tri_mesh_loader = avango.gua.nodes.TriMeshLoader()
-        cube = tri_mesh_loader.create_geometry_from_file("cube",
-                                            "data/objects/cube.obj",
-                                            avango.gua.LoaderFlags.NORMALIZE_POSITION
-                                            | avango.gua.LoaderFlags.NORMALIZE_SCALE)
-        cube.Transform.value = avango.gua.make_trans_mat(first_hit.WorldPosition.value) * avango.gua.make_scale_mat(0.01,0.01,0.01)
-        if self.SceneGraph.value != None:
-          self.SceneGraph.value.Root.value.Children.value.append(cube)
-          cubes.append(cube)
-        while len(cubes)>300:
-          tmp_cube = cubes.pop(0)
-          self.SceneGraph.value.Root.value.Children.value.remove(tmp_cube)
+      delta_trans = (new_pos.get_translate() - self.InTransform.value.get_translate())
 
-
-      new_pos = first_hit.WorldPosition.value
-      new_pos.y += self.OffsetToGround.value
-
-      result = (new_pos - in_translation)
-
-    self.OutTransform.value = self.OutTransform.value * avango.gua.make_trans_mat(result)
+    self.OutTransform.value = self.OutTransform.value * avango.gua.make_trans_mat(delta_trans/3.0)#/3.0 smoothing(???)
 
 
 def start():
@@ -108,7 +119,7 @@ def start():
 
   LAST_ANIMATION = bob.Animation.value
 
-  bob_nav.Transform.value =  bob_nav.Transform.value * avango.gua.make_trans_mat(0.0,0.05,0.0) * avango.gua.make_scale_mat(0.04,0.04,0.04)
+  bob_nav.Transform.value =  bob_nav.Transform.value * avango.gua.make_trans_mat(0.0,0.05,0.0) * avango.gua.make_scale_mat(0.2,0.2,0.2)
 
   bob.AnimationMode.value = 1
 
@@ -120,12 +131,10 @@ def start():
   tri_mesh_loader = avango.gua.nodes.TriMeshLoader()
 
   medieval_harbour = tri_mesh_loader.create_geometry_from_file("medieval_harbour", "/opt/3d_models/architecture/medieval_harbour/town.obj",
-                                            avango.gua.LoaderFlags.NORMALIZE_POSITION
-                                            | avango.gua.LoaderFlags.NORMALIZE_SCALE
-                                            | avango.gua.LoaderFlags.OPTIMIZE_GEOMETRY
-                                            | avango.gua.LoaderFlags.MAKE_PICKABLE)
+                                            avango.gua.LoaderFlags.MAKE_PICKABLE)
 
-  medieval_harbour.Transform.value = medieval_harbour.Transform.value * avango.gua.make_trans_mat(0,0.57, -5)
+  #medieval_harbour.Transform.value = medieval_harbour.Transform.value * avango.gua.make_trans_mat(0,0.57, -5)
+  #medieval_harbour.Transform.value = medieval_harbour.Transform.value * avango.gua.make_trans_mat(0.0,0.0, -5.0)
 
   plane = tri_mesh_loader.create_geometry_from_file("cube",
                                             "data/objects/cube.obj",
@@ -177,14 +186,14 @@ def start():
 
   cam.PipelineDescription.value = pipeline_description
 
-  cam.Transform.value = avango.gua.make_trans_mat(0.0, 0.0, 5.0)
+  cam.Transform.value = avango.gua.make_trans_mat(0.0, 0.0, 1.0)
   cam.FarClip.value = 10000
 
-  screen = avango.gua.nodes.ScreenNode(Name = "screen", Width = 1.6, Height = 0.9)
+  screen = avango.gua.nodes.ScreenNode(Name = "screen", Width = 0.8, Height = 0.45)
   screen.Children.value = [cam, light]
   screen.Transform.value = avango.gua.make_trans_mat(0, 0.1, -2)
 
-  graph.Root.value.Children.value = [bob_ground, medieval_harbour , plane ,screen,light2]
+  graph.Root.value.Children.value = [bob_ground, medieval_harbour ,screen,light2]
 
   avango.gua.register_window("window", window)
 
@@ -199,8 +208,8 @@ def start():
   character_control = CharacterControl()
   character_control.my_constructor(bob,bob_nav,window)
 
-  character_control.bind_transformation(65, avango.gua.make_rot_mat(2.0, 0.0, 1.0,0.0))
-  character_control.bind_transformation(68, avango.gua.make_rot_mat(-2.0, 0.0, 1.0,0.0))
+  character_control.bind_transformation(65, avango.gua.make_rot_mat(4.0, 0.0, 1.0,0.0))
+  character_control.bind_transformation(68, avango.gua.make_rot_mat(-4.0, 0.0, 1.0,0.0))
 
   character_control.key_down(87, "idle","run")
   character_control.key_up(87, "run","idle")
@@ -219,8 +228,8 @@ def start():
   # setup ground following
   ground_following = GroundFollowing(
     SceneGraph = graph,
-    OffsetToGround = 0.0,
-    MaxDistanceToGround = 10.3
+    OffsetToGround = 0.2,
+    MaxDistanceToGround = 1.0
   )
 
   ground_following.InTransform.connect_from(bob.WorldTransform)

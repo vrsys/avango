75885ac84378cd732ef2f2f1277dae027116c44b
Jakob Wagner <wobakjerna@web.de>
2015-03-24 17:50:46 +0100

    Improve evaluation in animation control

--- avango-skelanim/examples/skeletal_animation/AnimationControl.py
+++ avango-skelanim/examples/skeletal_animation/AnimationControl.py
@@ -1,11 +1,17 @@
 import avango
 import avango.script
+from enum import Enum
+
+class State(Enum):
+    blending = 0
+    play = 1
 
 class AnimationControl(avango.script.Script):
 
     _animation_node = None
     _timer = avango.nodes.TimeSensor()
-    _state = 1 # 1 = play; 2 = blending
+
+    _state = State.play
 
     _last_animation = None
     _current_animation = None
@@ -14,7 +20,7 @@ class AnimationControl(avango.script.Script):
     _last_looping = False
     _current_blending_start = 0.0
     _current_looping = False
-    _is_looping = False
+    _first_play = False
     
     _blending_factor = 1.0
     _blending_duration = 0.5
@@ -26,11 +32,11 @@ class AnimationControl(avango.script.Script):
         self.super(AnimationControl).__init__()
 
     def my_constructor(self, animation_node):
-
+        self._timer.ReferenceTime.value = self._timer.RealTime.value
         self._animation_node = animation_node
-
-        self.play("idle")
-        self.play("idle")
+        # set some anim because evaluate is called before anim can be set from outside
+        self.switch_to("idle")
+        self.switch_to("idle")
 
         self.always_evaluate(True)
 
@@ -47,9 +53,9 @@ class AnimationControl(avango.script.Script):
         return self._blending_factor
 
     def is_looping(self):
-        return self._is_looping
+        return self._first_play
 
-    def play(self, animation_name, loop_mode = True):
+    def switch_to(self, animation_name, loop_mode = True):
         self._last_animation = self._current_animation
         self._current_animation = animation_name
 
@@ -63,8 +69,13 @@ class AnimationControl(avango.script.Script):
         self._animation_node.Animation1.value = self._animation_node.Animation2.value
         self._animation_node.Animation2.value = animation_name
 
+        # reset time
+        self._timer.ReferenceTime.value = self._current_blending_start + self._timer.ReferenceTime.value
+        self._current_blending_start = 0
+
         self._animation_node.BlendFactor.value = self._blending_factor = 1.0
-        self._state = 1
+        
+        self._state = State.play
 
     def blend_to(self, animation_name, blending_duration = 0.5, loop_mode = True):
 
@@ -79,37 +90,40 @@ class AnimationControl(avango.script.Script):
 
         self._last_looping = self._current_looping
         self._current_looping = loop_mode
-        self._is_looping = True
+        self._first_play = True
 
         self._animation_node.Animation1.value = self._animation_node.Animation2.value
         self._animation_node.Animation2.value = animation_name
 
         self._animation_node.BlendFactor.value = self._blending_factor = (self._timer.Time.value - self._current_blending_start) / self._blending_duration
         
-        self._state = 2
+        self._state = State.blending
 
 
     def evaluate(self):
-        
-        if not self._last_looping and self._timer.Time.value > self._last_blending_start + self._animation_node.get_duration(self._last_animation):
-            self._animation_node.Time1.value = self._animation_node.get_duration(self._last_animation) * 0.999999
-        else:
-            self._animation_node.Time1.value = self._timer.Time.value - self._last_blending_start
 
+        # always update time of current anim
         if not self._current_looping and self._timer.Time.value > self._current_blending_start + self._animation_node.get_duration(self._current_animation):
             self._animation_node.Time2.value = self._animation_node.get_duration(self._current_animation) * 0.999999
-            self._is_looping = False
+            self._first_play = False
         else:
-            self._animation_node.Time2.value = self._timer.Time.value - self._current_blending_start
+            self._animation_node.Time2.value = (self._timer.Time.value - self._current_blending_start)
 
-        if self._state == 2:
+        if self._state == State.blending:
+            # update time for old animation only while blending
+            if not self._last_looping and self._timer.Time.value > self._last_blending_start + self._animation_node.get_duration(self._last_animation):
+                self._animation_node.Time1.value = self._animation_node.get_duration(self._last_animation) * 0.999999
+            else:
+                self._animation_node.Time1.value = (self._timer.Time.value - self._last_blending_start)
 
             self._blending_factor = 1.0 - (self._blending_end - self._timer.Time.value) / self._blending_duration
+            # if blending is finished, switch to normal play
             if self._blending_factor >= 1.0:
 
-                self._state = 1
+                self._state = State.play
                 self._blending_factor = 1.0
+                # reset time
+                self._timer.ReferenceTime.value = self._current_blending_start + self._timer.ReferenceTime.value
+                self._current_blending_start = 0
 
             self._animation_node.BlendFactor.value = self._blending_factor
-
-

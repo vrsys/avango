581d14f95d9fed13a61bb4e7c13a3d2822164b8b
Simon Schneegans <code@simonschneegans.de>
2015-04-13 15:22:08 +0200

    removed LightSourceSize

--- avango-gua/include/avango/gua/scenegraph/LightNode.hpp
+++ avango-gua/include/avango/gua/scenegraph/LightNode.hpp
@@ -58,7 +58,6 @@ namespace av
       SFBool  EnableSpecularShading;
       SFFloat ShadowOffset;
       SFUInt  ShadowMapSize;
-      SFFloat LightSourceSize;
 
       // new
       MFFloat ShadowCascadedSplits;
@@ -104,9 +103,6 @@ namespace av
       virtual void getShadowMapSizeCB(const SFUInt::GetValueEvent& event);
       virtual void setShadowMapSizeCB(const SFUInt::SetValueEvent& event);
       
-      virtual void getLightSourceSizeCB(const SFFloat::GetValueEvent& event);
-      virtual void setLightSourceSizeCB(const SFFloat::SetValueEvent& event);
-
       virtual void getShadowCascadedSplitsCB(const MFFloat::GetValueEvent& event);
       virtual void setShadowCascadedSplitsCB(const MFFloat::SetValueEvent& event);
 

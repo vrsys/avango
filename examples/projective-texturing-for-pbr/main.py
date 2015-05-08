7fda809f1b723fd6efb329709cc40101844afe8f
Jakob Harlan <jakob.harlan@uni-weimar.de>
2015-05-08 16:11:38 +0200

    Code cleanup and flake8

--- examples/projective-texturing-for-pbr/main.py
+++ examples/projective-texturing-for-pbr/main.py
@@ -1,65 +1,50 @@
 import avango
 import avango.script
-from avango.script import field_has_changed
 import avango.gua
 from examples_common.GuaVE import GuaVE
-from examples_common.navigator import Navigator
 
 from projector import XML_Projector
+
 
 def start():
 
     # setup scenegraph
-    graph  = avango.gua.nodes.SceneGraph(Name = "scenegraph")
+    graph = avango.gua.nodes.SceneGraph(Name="scenegraph")
     loader = avango.gua.nodes.TriMeshLoader()
 
+    # init Projector
     projector = XML_Projector()
     projector.load_XML("data/orthoimage.d.xml", graph.Root.value)
     projector.Texture.value = "data/textures/segMask.tga"
 
-    # monkey = loader.create_geometry_from_file("monkey", "data/objects/monkey.obj", avango.gua.LoaderFlags.NORMALIZE_SCALE)
-    # monkey.Material.connect_from(projector.Material)
-    # monkey.Transform.value = \
-    #     avango.gua.make_trans_mat(
-    #         499.211641143046,
-    #         -1095.8880106695,
-    #         130.737704808047,
-    #     )*\
-    #     avango.gua.make_scale_mat(0.1)
-
+    # init point cloud
     plodloader = avango.gua.nodes.PLODLoader()
     plodloader.UploadBudget.value = 32
     plodloader.RenderBudget.value = 2048
     plodloader.OutOfCoreBudget.value = 4096
     plod_node = plodloader.create_geometry_from_file(
-      "/home/yuqo8702/Desktop/Area-7_Rosa-Camuna_knn.kdn",
-      avango.gua.PLODLoaderFlags.DEFAULTS
-    )
-    plod_node.Material.connect_from(projector.Material)
-
-    light = avango.gua.nodes.LightNode(
-        Type=avango.gua.LightType.POINT,
-        Name = "light",
-        Color = avango.gua.Color(1.0, 1.0, 1.0),
-        Brightness = 50.0,
-        Transform = avango.gua.make_trans_mat(1, 1, 5) * avango.gua.make_scale_mat(15, 15, 15)
+        "/home/yuqo8702/Desktop/Area-7_Rosa-Camuna_knn.kdn",
+        avango.gua.PLODLoaderFlags.DEFAULTS,
     )
 
-    size = avango.gua.Vec2ui(1024, 768)
+    plod_node.Material.value = projector.Material.value
+
+    # setup viewing
+    size = avango.gua.Vec2ui(1920, 1080)
 
     window = avango.gua.nodes.GlfwWindow(
-        Size = size,
-        LeftResolution = size
+        Size=size,
+        LeftResolution=size
     )
 
     avango.gua.register_window("window", window)
 
     cam = avango.gua.nodes.CameraNode(
-        LeftScreenPath = "/screen",
-        SceneGraph = "scenegraph",
-        Resolution = size,
-        OutputWindowName = "window",
-        Transform = avango.gua.make_trans_mat(0.0, 0.0, 2.0),
+        LeftScreenPath="/screen",
+        SceneGraph="scenegraph",
+        Resolution=size,
+        OutputWindowName="window",
+        Transform=avango.gua.make_trans_mat(0.0, 0.0, 2.0),
     )
 
     pipeline_description = avango.gua.nodes.PipelineDescription(
@@ -73,11 +58,10 @@ def start():
     cam.PipelineDescription.value = pipeline_description
 
     screen = avango.gua.nodes.ScreenNode(
-        Name = "screen",
-        Width = 0.4,
-        Height = 0.3,
-        Children = [cam],
-        # Transform=avango.gua.make_identity_mat(),
+        Name="screen",
+        Width=0.64,
+        Height=0.36,
+        Children=[cam],
         Transform=avango.gua.make_trans_mat(
             499.211641143046,
             -1095.8880106695,
@@ -85,23 +69,12 @@ def start():
         ),
     )
 
-    graph.Root.value.Children.value = [light, screen, projector.group_node, plod_node]
+    graph.Root.value.Children.value = [screen, projector.group_node, plod_node]
 
     #setup viewer
     viewer = avango.gua.nodes.Viewer()
     viewer.SceneGraphs.value = [graph]
     viewer.Windows.value = [window]
-
-    # # projector movement
-    # navigator = Navigator()
-    # navigator.OutTransform.value = avango.gua.make_identity_mat()
-    # navigator.StartLocation.value = avango.gua.Vec3()
-
-    # navigator.RotationSpeed.value = 0.2
-    # navigator.MotionSpeed.value = 0.04
-
-    # monkey.Transform.connect_from(navigator.OutTransform)
-
 
     guaVE = GuaVE()
     guaVE.start(locals(), globals())
@@ -109,7 +82,5 @@ def start():
     viewer.run()
 
 
-
 if __name__ == '__main__':
     start()
-
--- examples/projective-texturing-for-pbr/projector.py
+++ examples/projective-texturing-for-pbr/projector.py
@@ -4,41 +4,57 @@ import avango.gua
 from avango.script import field_has_changed
 
 import xml.etree.ElementTree as ET
-from decimal import Decimal
+
 
 class XML_Projector(avango.script.Script):
-
     Texture = avango.SFString()
     Material = avango.gua.SFMaterial()
     Image_View_Mat = avango.gua.SFMatrix4()
 
     def __init__(self):
         self.super(XML_Projector).__init__()
-    
-    def load_XML(self, XML_path, parent):
         loader = avango.gua.nodes.TriMeshLoader()
-        xml = ET.parse(XML_path).getroot()
 
-        self.group_node = avango.gua.nodes.TransformNode(Name = "projector_group")
-        parent.Children.value.append(self.group_node)
-        self.group_node.Transform.value = self.load_transform(xml)
+        # setup node and frame geometry
+        self.group_node = avango.gua.nodes.TransformNode(
+            Name="projector_group"
+        )
         self.geometry = loader.create_geometry_from_file(
             "frame",
             "data/objects/frame.obj",
             avango.gua.LoaderFlags.DEFAULTS,
         )
-        self.geometry.Material.value.set_uniform("Color", avango.gua.Vec4(1.0, 1.0, 1.0, 1.0))
-        self.geometry.Material.value.set_uniform("Emissivity", 0.3)
+        self.geometry.Material.value.set_uniform(
+            "Color",
+            avango.gua.Vec4(1.0, 1.0, 1.0, 1.0),
+        )
+        self.geometry.Material.value.set_uniform("Emissivity", 0.5)
         self.group_node.Children.value.append(self.geometry)
 
+        # setup Material
         self.Material.value = avango.gua.nodes.Material(
-          ShaderName = "proj_mat"
+            ShaderName="proj_mat"
         )
         proj_mat_desc = avango.gua.nodes.MaterialShaderDescription()
-        proj_mat_desc.load_from_file("data/materials/Projective_Texture_pbr.gmd")
+        proj_mat_desc.load_from_file(
+            "data/materials/Projective_Texture_pbr.gmd"
+        )
         avango.gua.register_material_shader(proj_mat_desc, "proj_mat")
-        self.Image_View_Mat.value = avango.gua.make_inverse_mat(self.group_node.Transform.value)
-        self.Material.value.set_uniform("Image_View_Mat", self.Image_View_Mat.value)
+
+    def load_XML(self, XML_path, parent):
+        xml = ET.parse(XML_path).getroot()
+
+        # load Transformation from XML
+        self.group_node.Transform.value = self.load_transform(xml)
+        parent.Children.value.append(self.group_node)
+
+        # set Matrix
+        self.Image_View_Mat.value =\
+            avango.gua.make_inverse_mat(self.group_node.Transform.value)
+        self.Material.value.set_uniform(
+            "Image_View_Mat",
+            self.Image_View_Mat.value,
+        )
 
     def load_transform(self, xml):
         trans = xml.find("Transformation")
@@ -46,13 +62,13 @@ class XML_Projector(avango.script.Script):
         rot_x = parse_vec3(trans.find("Direction").find("X").text)
         rot_y = parse_vec3(trans.find("Direction").find("Y").text)
         rot_z = parse_vec3(trans.find("Direction").find("Z").text)
-        
+
         pxl = xml.find("Image").find("Pixel")
         pxl_size = float(pxl.find("Size").text)
 
         pxl_x = float(pxl.find("Width").text)
         pxl_y = float(pxl.find("Height").text)
-        
+
         scale_x = pxl_x * pxl_size
         scale_y = pxl_y * pxl_size
 
@@ -67,7 +83,10 @@ class XML_Projector(avango.script.Script):
 
     @field_has_changed(Texture)
     def update_texture(self):
-        self.Material.value.set_uniform("projective_texture", self.Texture.value)
+        self.Material.value.set_uniform(
+            "projective_texture",
+            self.Texture.value,
+        )
 
 
 def parse_vec3(text, faktor=1.0):
@@ -85,76 +104,3 @@ def build_matrix(p, x, y, z):
         p[0], p[1], p[2], 1.0,
     ]
     return avango.gua.from_list(lst)
-
-
-class Projector(avango.script.Script):
-
-  Material = avango.gua.SFMaterial()
-  Transform = avango.gua.SFMatrix4()
-  Graph = avango.gua.SFSceneGraph()
-
-  Texture = avango.SFString()
-
-
-  def __init__(self):
-    self.super(Projector).__init__()
-    loader = avango.gua.nodes.TriMeshLoader()
-
-    self.group_node = avango.gua.nodes.TransformNode(Name = "projector_group")
-    self.group_node.Transform.connect_from(self.Transform)
-
-    self.geometry = loader.create_geometry_from_file(
-        "projector_geometry",
-        "data/objects/projector.obj",
-        avango.gua.LoaderFlags.NORMALIZE_SCALE |
-            avango.gua.LoaderFlags.NORMALIZE_POSITION |
-            avango.gua.LoaderFlags.LOAD_MATERIALS,
-    )
-    self.geometry.Transform.value = avango.gua.make_scale_mat(0.03)
-    self.group_node.Children.value.append(self.geometry)
-
-    self.screen = avango.gua.nodes.ScreenNode(
-      Name = "screen",
-      Width = 0.5,
-      Height = 0.5,
-      Transform = avango.gua.make_trans_mat(0.0, 0.0, -0.1)
-    )
-    self.group_node.Children.value.append(self.screen)
-
-    self.cam = avango.gua.nodes.CameraNode(
-      LeftScreenPath = "projector_group/screen",
-      SceneGraph = "scenegraph",
-    )
-    self.group_node.Children.value.append(self.cam)
-
-    self.Material.value = avango.gua.nodes.Material(
-      ShaderName = "proj_mat"
-    )
-
-    self.Graph.value = avango.gua.nodes.SceneGraph(Name = "dummy")
-
-    proj_mat_desc = avango.gua.nodes.MaterialShaderDescription()
-    proj_mat_desc.load_from_file("data/materials/Projective_Texture_pbr.gmd")
-    avango.gua.register_material_shader(proj_mat_desc, "proj_mat")
-
-
-  def set_scenegraph(self, graph):
-    self.graph = graph
-
-  @field_has_changed(Transform)
-  def update_matrices(self):
-    frustum = self.cam.get_frustum(self.Graph.value, avango.gua.CameraMode.CENTER)
-
-    projection_matrix = frustum.ProjectionMatrix.value
-    view_matrix = frustum.ViewMatrix.value
-    
-    self.Material.value.set_uniform("projective_texture_matrix", projection_matrix)
-    self.Material.value.set_uniform("view_texture_matrix", view_matrix ) 
-
-
-  @field_has_changed(Texture)
-  def update_texture(self):
-    self.Material.value.set_uniform("projective_texture", self.Texture.value)
-
-
-

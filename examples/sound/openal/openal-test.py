5087e81ea6742b7f9d7c51624bfacbc1653960d9
Andreas-Christoph Bernstein <andreas.bernstein@uni-weimar.de>
2015-06-19 11:02:26 +0200

    Update sound example

--- examples/sound/openal/openal-test.py
+++ examples/sound/openal/openal-test.py
@@ -1,27 +1,5 @@
-# -*- Mode:Python -*-
-
-##########################################################################
-#                                                                        #
-# This file is part of AVANGO.                                           #
-#                                                                        #
-# Copyright 1997 - 2009 Fraunhofer-Gesellschaft zur Foerderung der       #
-# angewandten Forschung (FhG), Munich, Germany.                          #
-#                                                                        #
-# AVANGO is free software: you can redistribute it and/or modify         #
-# it under the terms of the GNU Lesser General Public License as         #
-# published by the Free Software Foundation, version 3.                  #
-#                                                                        #
-# AVANGO is distributed in the hope that it will be useful,              #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of         #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the           #
-# GNU General Public License for more details.                           #
-#                                                                        #
-# You should have received a copy of the GNU Lesser General Public       #
-# License along with AVANGO. If not, see <http://www.gnu.org/licenses/>. #
-#                                                                        #
-##########################################################################
-
 import avango
+import avango.script
 import avango.gua
 import avango.sound
 import avango.sound.openal
@@ -32,78 +10,145 @@ openal_renderer = avango.sound.openal.nodes.OpenALSoundRenderer()
 openal_renderer.Device.value = ""
 soundtraverser.Renderers.value = [openal_renderer]
 
-# set up scene graph
-avango.gua.load_shading_models_from("data/materials")
-avango.gua.load_materials_from("data/materials")
 
-graph = avango.gua.nodes.SceneGraph(Name = "scenegraph")
+class TimedRotate(avango.script.Script):
+    TimeIn = avango.SFFloat()
+    MatrixOut = avango.gua.SFMatrix4()
+
+    def evaluate(self):
+        self.MatrixOut.value = avango.gua.make_rot_mat(
+            self.TimeIn.value * 2.0, 0.0, 1.0, 0.0)
+
+
+# setup scenegraph
+graph = avango.gua.nodes.SceneGraph(Name="scenegraph")
 loader = avango.gua.nodes.TriMeshLoader()
-obj = loader.create_geometry_from_file("monkey",
-                                      "data/objects/monkey.obj",
-                                      "Stones",
-                                      avango.gua.LoaderFlags.DEFAULTS)
-obj_trans = avango.gua.nodes.TransformNode()
-obj_trans.Children.value = [obj]
+
+monkey1 = loader.create_geometry_from_file(
+    "monkey",
+    "data/objects/monkey.obj",
+    avango.gua.LoaderFlags.NORMALIZE_SCALE)
+
+monkey2 = loader.create_geometry_from_file(
+    "monkey",
+    "data/objects/monkey.obj",
+    avango.gua.LoaderFlags.NORMALIZE_SCALE)
+
+monkey1.Material.value.set_uniform(
+    "Color",
+    avango.gua.Vec4(1.0, 0.766, 0.336, 1.0))
+monkey1.Material.value.set_uniform("Roughness", 0.3)
+monkey1.Material.value.set_uniform("Metalness", 1.0)
+
+monkey2.Material.value.set_uniform(
+    "Color", avango.gua.Vec4(1.0, 0.266, 0.136, 1.0))
+monkey2.Material.value.set_uniform("Roughness", 0.6)
+monkey2.Material.value.set_uniform("Metalness", 0.0)
+
+transform1 = avango.gua.nodes.TransformNode(
+    Children=[monkey1]
+    )
+transform2 = avango.gua.nodes.TransformNode(
+    Transform=avango.gua.make_trans_mat(-0.5, 0.0, 0.0),
+    Children=[monkey2]
+    )
+
+light = avango.gua.nodes.LightNode(
+    Type=avango.gua.LightType.POINT,
+    Name="light",
+    Color=avango.gua.Color(1.0, 1.0, 1.0),
+    Brightness=100.0,
+    Transform=(avango.gua.make_trans_mat(1, 1, 5) *
+               avango.gua.make_scale_mat(30, 30, 30))
+    )
+
+size = avango.gua.Vec2ui(1024, 768)
+
+window = avango.gua.nodes.GlfwWindow(
+    Size=size,
+    LeftResolution=size
+    )
+
+avango.gua.register_window("window", window)
+
+cam = avango.gua.nodes.CameraNode(
+    LeftScreenPath="/sound/screen",
+    SceneGraph="scenegraph",
+    Resolution=size,
+    OutputWindowName="window",
+    Transform=avango.gua.make_trans_mat(0.0, 0.0, 3.5)
+    )
+
+res_pass = avango.gua.nodes.ResolvePassDescription()
+res_pass.EnableSSAO.value = True
+res_pass.SSAOIntensity.value = 4.0
+res_pass.SSAOFalloff.value = 10.0
+res_pass.SSAORadius.value = 7.0
+
+#res_pass.EnableScreenSpaceShadow.value = True
+
+res_pass.EnvironmentLightingColor.value = avango.gua.Color(0.1, 0.1, 0.1)
+res_pass.ToneMappingMode.value = avango.gua.ToneMappingMode.UNCHARTED
+res_pass.Exposure.value = 1.0
+res_pass.BackgroundColor.value = avango.gua.Color(0.45, 0.5, 0.6)
+
+anti_aliasing = avango.gua.nodes.SSAAPassDescription()
+
+pipeline_description = avango.gua.nodes.PipelineDescription(
+    Passes=[
+        avango.gua.nodes.TriMeshPassDescription(),
+        avango.gua.nodes.LightVisibilityPassDescription(),
+        res_pass,
+        anti_aliasing,
+        ])
+
+cam.PipelineDescription.value = pipeline_description
+
+screen = avango.gua.nodes.ScreenNode(
+    Name="screen",
+    Width=2,
+    Height=1.5,
+    Children=[cam]
+    )
 
 #set up sound
 stereosound = avango.sound.nodes.SoundSource()
-obj_trans.Children.value.append(stereosound)
+transform1.Children.value.append(stereosound)
 stereosound.URL.value = "applause.ogg"
 stereosound.Loop.value = False
 
 monosound = avango.sound.nodes.SoundSource()
-obj_trans.Children.value.append(monosound)
+transform1.Children.value.append(monosound)
 monosound.URL.value = "applause_mono.ogg"
 monosound.Loop.value = False
 
 root_group = avango.gua.nodes.TransformNode(Name="sound")
-
-light = avango.gua.nodes.PointLightNode(Name = "light",
-                                        Color = avango.gua.Color(1.0, 1.0, 1.0))
-light.Transform.value = avango.gua.make_trans_mat(1, 1, 2) * avango.gua.make_scale_mat(15, 15, 15)
-
-eye = avango.gua.nodes.TransformNode(Name = "eye")
-eye.Transform.value = avango.gua.make_trans_mat(0.0, 0.0, 3.5)
-
-screen = avango.gua.nodes.ScreenNode(Name = "screen", Width = 4, Height = 3)
-screen.Children.value = [eye]
-
-root_group.Children.value = [obj_trans, light, screen]
+root_group.Children.value = [transform1, transform2, light, screen]
 graph.Root.value.Children.value = [root_group]
 
-# set up viewing
-size = avango.gua.Vec2ui(1024, 768)
-pipe = avango.gua.nodes.Pipeline(
-          Camera = avango.gua.nodes.Camera(
-            LeftEye = "/sound/screen/eye",
-            RightEye = "/sound/screen/eye",
-            LeftScreen = "/sound/screen",
-            RightScreen = "/sound/screen",
-            SceneGraph = "scenegraph"),
-          Window = avango.gua.nodes.Window(Size = size,
-                                           LeftResolution = size),
-          LeftResolution = size,
-          BackgroundMode = avango.gua.BackgroundMode.COLOR
-          )
-
-pipe.Enabled.value = True
-
 #setup viewer
-
 viewer = avango.gua.nodes.Viewer()
-viewer.Pipelines.value = [pipe]
 viewer.SceneGraphs.value = [graph]
+viewer.Windows.value = [window]
+
+monkey_updater = TimedRotate()
+
+timer = avango.nodes.TimeSensor()
+monkey_updater.TimeIn.connect_from(timer.Time)
+
+transform1.Transform.connect_from(monkey_updater.MatrixOut)
+
+guaVE = GuaVE()
+guaVE.start(locals(), globals())
 
 soundtraverser.RootNode.value = root_group
 soundtraverser.Traverse.value = True
 
-openal_renderer.ListenerPosition.connect_from(eye.Transform)
+openal_renderer.ListenerPosition.connect_from(cam.Transform)
+
+viewer.frame()
 
 stereosound.Play.value = True
-monosound.Play.value = True
+#monosound.Play.value = True
 
-#viewer.frame()
-
-guaVE = GuaVE()
-guaVE.start(locals(), globals())
 viewer.run()

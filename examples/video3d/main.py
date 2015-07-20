e7ff9786f67ee126052fe58aff18de96b8629755
Andreas-Christoph Bernstein <andreas.bernstein@uni-weimar.de>
2015-07-20 13:32:30 +0200

    Update video3d example

--- examples/video3d/main.py
+++ examples/video3d/main.py
@@ -5,58 +5,104 @@ import avango.gua
 from examples_common.GuaVE import GuaVE
 import sys
 
-class TimedRotate(avango.script.Script):
-  TimeIn = avango.SFFloat()
-  MatrixOut = avango.gua.SFMatrix4()
 
-  @field_has_changed(TimeIn)
-  def update(self):
-    self.MatrixOut.value = avango.gua.make_rot_mat(self.TimeIn.value*2.0, 0.0, 1.0, 0.0)
+class TimedRotate(avango.script.Script):
+    TimeIn = avango.SFFloat()
+    MatrixOut = avango.gua.SFMatrix4()
+
+    def evaluate(self):
+        self.MatrixOut.value = avango.gua.make_trans_mat(0.0, -1.0, -2.0) * avango.gua.make_rot_mat( 10*self.TimeIn.value * 2.0, 0.0, 1.0, 0.0)
+
 
 def start(filename):
+    # setup scenegraph
+    graph = avango.gua.nodes.SceneGraph(Name="scenegraph")
+    loader = avango.gua.nodes.TriMeshLoader()
 
-  # setup scenegraph
-  graph = avango.gua.nodes.SceneGraph(Name = "scenegraph")
+    videoloader = avango.gua.nodes.Video3DLoader()
+    video_geode = videoloader.load("kinect", filename)
 
-  videoloader = avango.gua.nodes.Video3DLoader()
-  video_geode = videoloader.load("kinect", filename)
+    transform1 = avango.gua.nodes.TransformNode(
+        Children=[video_geode]
+        )
 
-  light = avango.gua.nodes.LightNode(
-    Type=avango.gua.LightType.POINT,
-    Name = "light", Color = avango.gua.Color(1.0, 1.0, 1.0))
-  light.Transform.value = avango.gua.make_trans_mat(1, 1, 2) * avango.gua.make_scale_mat(15, 15, 15)
+    light = avango.gua.nodes.LightNode(
+        Type=avango.gua.LightType.POINT,
+        Name="light",
+        Color=avango.gua.Color(1.0, 1.0, 1.0),
+        Brightness=100.0,
+        Transform=(avango.gua.make_trans_mat(1, 1, 5) *
+                   avango.gua.make_scale_mat(30, 30, 30))
+        )
 
-  eye = avango.gua.nodes.TransformNode(Name = "eye")
-  eye.Transform.value = avango.gua.make_trans_mat(0.0, 0.0, 3.5)
+    size = avango.gua.Vec2ui(1024, 768)
 
-  screen = avango.gua.nodes.ScreenNode(Name = "screen", Width = 4, Height = 3)
-  screen.Children.value = [eye]
+    window = avango.gua.nodes.GlfwWindow(
+        Size=size,
+        LeftResolution=size
+        )
 
-  graph.Root.value.Children.value = [video_geode, light, screen]
+    avango.gua.register_window("window", window)
 
-  # setup viewing
-  size = avango.gua.Vec2ui(1024, 768)
-  pipe = avango.gua.nodes.Pipeline(Camera = avango.gua.nodes.Camera(LeftEye = "/screen/eye",
-                                                                    RightEye = "/screen/eye",
-                                                                    LeftScreen = "/screen",
-                                                                    RightScreen = "/screen",
-                                                                    SceneGraph = "scenegraph"),
-                                   Window = avango.gua.nodes.Window(Size = size,
-                                                                    LeftResolution = size),
-                                   LeftResolution = size)
-  pipe.EnableBackfaceCulling.value = False
+    cam = avango.gua.nodes.CameraNode(
+        LeftScreenPath="/screen",
+        SceneGraph="scenegraph",
+        Resolution=size,
+        OutputWindowName="window",
+        Transform=avango.gua.make_trans_mat(0.0, 0.0, 3.5)
+        )
 
-  #setup viewer
-  viewer = avango.gua.nodes.Viewer()
-  viewer.Pipelines.value = [pipe]
-  viewer.SceneGraphs.value = [graph]
+    res_pass = avango.gua.nodes.ResolvePassDescription()
+    res_pass.EnableSSAO.value = True
+    res_pass.SSAOIntensity.value = 4.0
+    res_pass.SSAOFalloff.value = 10.0
+    res_pass.SSAORadius.value = 7.0
 
-  timer = avango.nodes.TimeSensor()
+    #res_pass.EnableScreenSpaceShadow.value = True
 
-  guaVE = GuaVE()
-  guaVE.start(locals(), globals())
+    res_pass.EnvironmentLightingColor.value = avango.gua.Color(0.1, 0.1, 0.1)
+    res_pass.ToneMappingMode.value = avango.gua.ToneMappingMode.UNCHARTED
+    res_pass.Exposure.value = 1.0
+    res_pass.BackgroundColor.value = avango.gua.Color(0.45, 0.5, 0.6)
 
-  viewer.run()
+    anti_aliasing = avango.gua.nodes.SSAAPassDescription()
+
+    pipeline_description = avango.gua.nodes.PipelineDescription(
+        Passes=[
+            avango.gua.nodes.TriMeshPassDescription(),
+            avango.gua.nodes.LightVisibilityPassDescription(),
+            avango.gua.nodes.Video3DPassDescription(),
+            res_pass,
+            anti_aliasing,
+            ])
+
+    cam.PipelineDescription.value = pipeline_description
+
+    screen = avango.gua.nodes.ScreenNode(
+        Name="screen",
+        Width=2,
+        Height=1.5,
+        Children=[cam]
+        )
+
+    graph.Root.value.Children.value = [transform1, light, screen]
+
+    #setup viewer
+    viewer = avango.gua.nodes.Viewer()
+    viewer.SceneGraphs.value = [graph]
+    viewer.Windows.value = [window]
+
+    monkey_updater = TimedRotate()
+
+    timer = avango.nodes.TimeSensor()
+    monkey_updater.TimeIn.connect_from(timer.Time)
+
+    transform1.Transform.connect_from(monkey_updater.MatrixOut)
+
+    guaVE = GuaVE()
+    guaVE.start(locals(), globals())
+
+    viewer.run()
 
 
 if __name__ == '__main__':

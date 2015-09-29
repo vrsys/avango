1273b4351b3109cc86769729d897895943bb5861
thelaui <felix.lauer@uni-weimar.de>
2015-09-29 12:51:34 +0200

    implemented experimental active stereo support

--- avango-gua/include/avango/gua/renderer/WindowBase.hpp
+++ avango-gua/include/avango/gua/renderer/WindowBase.hpp
@@ -31,7 +31,8 @@ namespace av
         SIDE_BY_SIDE = static_cast<unsigned>(::gua::StereoMode::SIDE_BY_SIDE),
         ANAGLYPH_RED_GREEN = static_cast<unsigned>(::gua::StereoMode::ANAGLYPH_RED_GREEN),
         ANAGLYPH_RED_CYAN = static_cast<unsigned>(::gua::StereoMode::ANAGLYPH_RED_CYAN),
-        CHECKERBOARD = static_cast<unsigned>(::gua::StereoMode::CHECKERBOARD)
+        CHECKERBOARD = static_cast<unsigned>(::gua::StereoMode::CHECKERBOARD),
+        ACTIVE = static_cast<unsigned>(::gua::StereoMode::ACTIVE)
       };
 
       /**

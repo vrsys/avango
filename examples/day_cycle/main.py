21a892a6f65ca240e24999afb55a5c92cf322c2e
Andreas-Christoph Bernstein <andreas.bernstein@uni-weimar.de>
2016-06-08 15:16:23 +0200

    Format code

--- examples/day_cycle/main.py
+++ examples/day_cycle/main.py
@@ -2,6 +2,7 @@ import avango
 import avango.script
 from avango.script import field_has_changed
 import avango.gua
+import avango.gua as av
 import avango.gua.gui
 from examples_common.GuaVE import GuaVE
 import examples_common.navigator
@@ -27,9 +28,9 @@ def get_elevation(city, time):
 
 class FPSUpdater(avango.script.Script):
     TimeIn = avango.SFFloat()
-    FPSResource = avango.gua.gui.SFGuiResourceNode()
-    Window = avango.gua.SFWindowBase()
-    Viewer = avango.gua.SFViewer()
+    FPSResource = av.gui.SFGuiResourceNode()
+    Window = av.SFWindowBase()
+    Viewer = av.SFViewer()
 
     @field_has_changed(TimeIn)
     def update_fps(self):
@@ -46,10 +47,10 @@ class SunUpdater(avango.script.Script):
     TimeIn = avango.SFFloat()
     TimeScale = avango.SFFloat()
 
-    MatrixOut = avango.gua.SFMatrix4()
-    DirectionOut = avango.gua.SFVec3()
-    SunColorOut = avango.gua.SFColor()
-    GroundColorOut = avango.gua.SFColor()
+    MatrixOut = av.SFMatrix4()
+    DirectionOut = av.SFVec3()
+    SunColorOut = av.SFColor()
+    GroundColorOut = av.SFColor()
     BlendFactorOut = avango.SFFloat()
 
     def __init__(self):
@@ -71,12 +72,11 @@ class SunUpdater(avango.script.Script):
             g = pow(g, 0.4) * 1.2
             b = pow(b, 0.5) * 1.6
 
-        self.SunColorOut.value = avango.gua.Color(r, g, b)
-        self.MatrixOut.value = avango.gua.make_rot_mat(
-            azimuth, 0, 1, 0) * avango.gua.make_rot_mat(-elevation, 1, 0, 0)
-        direcion = self.MatrixOut.value * avango.gua.Vec3(0, 0, -1)
-        self.DirectionOut.value = avango.gua.Vec3(direcion.x, direcion.y,
-                                                  direcion.z)
+        self.SunColorOut.value = av.Color(r, g, b)
+        self.MatrixOut.value = av.make_rot_mat(
+            azimuth, 0, 1, 0) * av.make_rot_mat(-elevation, 1, 0, 0)
+        direcion = self.MatrixOut.value * av.Vec3(0, 0, -1)
+        self.DirectionOut.value = av.Vec3(direcion.x, direcion.y, direcion.z)
 
         self.BlendFactorOut.value = 0.0
 
@@ -85,33 +85,31 @@ class SunUpdater(avango.script.Script):
 
         if direcion.y > 0:
             val = max(0.2 - direcion.y, 0)
-            self.GroundColorOut.value = avango.gua.Color(val, val, val)
+            self.GroundColorOut.value = av.Color(val, val, val)
         else:
-            self.GroundColorOut.value = avango.gua.Color(0.5, 0.5, 0.5)
+            self.GroundColorOut.value = av.Color(0.5, 0.5, 0.5)
 
 
 def start():
-    graph = avango.gua.nodes.SceneGraph(Name="scene")
-    loader = avango.gua.nodes.TriMeshLoader()
+    graph = av.nodes.SceneGraph(Name="scene")
+    loader = av.nodes.TriMeshLoader()
 
-    fps_size = avango.gua.Vec2(170, 55)
+    fps_size = av.Vec2(170, 55)
 
-    fps = avango.gua.gui.nodes.GuiResourceNode()
+    fps = av.gui.nodes.GuiResourceNode()
     fps.TextureName.value = "fps"
     fps.URL.value = "asset://gua/data/html/fps.html"
     fps.Size.value = fps_size
 
-    fps_quad = avango.gua.nodes.TexturedScreenSpaceQuadNode(
-        Name="fps_quad",
-        Texture="fps",
-        Width=int(fps_size.x),
-        Height=int(fps_size.y),
-        Anchor=avango.gua.Vec2(1.0, 1.0))
+    fps_quad = av.nodes.TexturedScreenSpaceQuadNode(Name="fps_quad",
+                                                    Texture="fps",
+                                                    Width=int(fps_size.x),
+                                                    Height=int(fps_size.y),
+                                                    Anchor=av.Vec2(1.0, 1.0))
     graph.Root.value.Children.value.append(fps_quad)
 
-    fallback_mat = avango.gua.create_material(
-        avango.gua.MaterialCapabilities.COLOR_VALUE |
-        avango.gua.MaterialCapabilities.ROUGHNESS_VALUE)
+    fallback_mat = av.create_material(av.MaterialCapabilities.COLOR_VALUE |
+                                      av.MaterialCapabilities.ROUGHNESS_VALUE)
     fallback_mat.set_uniform("Roughness", 0.6)
 
     for x in range(0, CUBE_COUNT_X):
@@ -121,80 +119,76 @@ def start():
                 new_cube = loader.create_geometry_from_file(
                     "cube" + str(x) + str(y) + str(z),
                     "data/objects/monkey.obj", fallback_mat,
-                    avango.gua.LoaderFlags.DEFAULTS)
+                    av.LoaderFlags.DEFAULTS)
 
-                new_cube.Transform.value = avango.gua.make_trans_mat(x*2, y*2, z*2) * \
-                                   avango.gua.make_scale_mat(0.3, 0.3, 0.3)
+                new_cube.Transform.value = av.make_trans_mat(
+                    x * 2, y * 2, z * 2) * av.make_scale_mat(0.3, 0.3, 0.3)
                 graph.Root.value.Children.value.append(new_cube)
                 new_cube.ShadowMode.value = 1
 
-    sun_light = avango.gua.nodes.LightNode(
-        Name="sun_light",
-        Type=avango.gua.LightType.SUN,
-        Color=avango.gua.Color(1.0, 1.0, 0.7),
-        EnableShadows=True,
-        ShadowMapSize=1024,
-        ShadowOffset=0.0005,
-        ShadowCascadedSplits=[0.1, 4, 7, 20],
-        ShadowMaxDistance=30,
-        ShadowNearClippingInSunDirection=100,
-        ShadowFarClippingInSunDirection=100,
-        Brightness=4)
+    sun_light = av.nodes.LightNode(Name="sun_light",
+                                   Type=av.LightType.SUN,
+                                   Color=av.Color(1.0, 1.0, 0.7),
+                                   EnableShadows=True,
+                                   ShadowMapSize=1024,
+                                   ShadowOffset=0.0005,
+                                   ShadowCascadedSplits=[0.1, 4, 7, 20],
+                                   ShadowMaxDistance=30,
+                                   ShadowNearClippingInSunDirection=100,
+                                   ShadowFarClippingInSunDirection=100,
+                                   Brightness=4)
     graph.Root.value.Children.value.append(sun_light)
 
-    floor = loader.create_geometry_from_file(
-        "floor", "data/objects/plane.obj", fallback_mat,
-        avango.gua.LoaderFlags.DEFAULTS)
-    floor.Transform.value = avango.gua.make_scale_mat(
-        200, 1, 200) * avango.gua.make_trans_mat(-0.5, -0.2, -0.5)
+    floor = loader.create_geometry_from_file("floor", "data/objects/plane.obj",
+                                             fallback_mat,
+                                             av.LoaderFlags.DEFAULTS)
+    floor.Transform.value = av.make_scale_mat(
+        200, 1, 200) * av.make_trans_mat(-0.5, -0.2, -0.5)
     floor.ShadowMode.value = 0
     graph.Root.value.Children.value.append(floor)
 
     width = 1920
     height = int(width * 9.0 / 16.0)
-    size = avango.gua.Vec2ui(width, height)
+    size = av.Vec2ui(width, height)
 
-    screen = avango.gua.nodes.ScreenNode(
-        Name="screen",
-        Width=4.8,
-        Height=2.7,
-        Transform=avango.gua.make_trans_mat(0.0, 0.0, -2.5))
+    screen = av.nodes.ScreenNode(Name="screen",
+                                 Width=4.8,
+                                 Height=2.7,
+                                 Transform=av.make_trans_mat(0.0, 0.0, -2.5))
 
-    camera = avango.gua.nodes.CameraNode(
-        Name="cam",
-        LeftScreenPath="/cam/screen",
-        RightScreenPath="/cam/screen",
-        SceneGraph="scene",
-        Resolution=size,
-        OutputWindowName="window",
-        EyeDistance=0.2,
-        EnableStereo=False,
-        Children=[screen],
-        Transform=avango.gua.make_trans_mat(0.0, 0.0, 7.0))
+    camera = av.nodes.CameraNode(Name="cam",
+                                 LeftScreenPath="/cam/screen",
+                                 RightScreenPath="/cam/screen",
+                                 SceneGraph="scene",
+                                 Resolution=size,
+                                 OutputWindowName="window",
+                                 EyeDistance=0.2,
+                                 EnableStereo=False,
+                                 Children=[screen],
+                                 Transform=av.make_trans_mat(0.0, 0.0, 7.0))
 
-    res_pass = avango.gua.nodes.ResolvePassDescription()
+    res_pass = av.nodes.ResolvePassDescription()
     res_pass.EnableSSAO.value = True
     res_pass.SSAOIntensity.value = 3.0
     res_pass.SSAOFalloff.value = 20.0
     res_pass.SSAORadius.value = 10.0
     res_pass.EnvironmentLightingTexture.value = "day_skymap"
     res_pass.AlternativeEnvironmentLightingTexture.value = "night_skymap"
-    res_pass.EnvironmentLightingMode.value = avango.gua.EnvironmentLightingMode.CUBEMAP
-    res_pass.ToneMappingMode.value = avango.gua.ToneMappingMode.UNCHARTED
+    res_pass.EnvironmentLightingMode.value = av.EnvironmentLightingMode.CUBEMAP
+    res_pass.ToneMappingMode.value = av.ToneMappingMode.UNCHARTED
     res_pass.Exposure.value = 1.0
-    res_pass.VignetteColor.value = avango.gua.Vec4(0, 0, 0, 0.7)
+    res_pass.VignetteColor.value = av.Vec4(0, 0, 0, 0.7)
     res_pass.VignetteCoverage.value = 0.0
     res_pass.EnableFog.value = True
     res_pass.FogStart.value = 30
     res_pass.FogEnd.value = 100
-    res_pass.BackgroundMode.value = avango.gua.BackgroundMode.CUBEMAP_TEXTURE
+    res_pass.BackgroundMode.value = av.BackgroundMode.CUBEMAP_TEXTURE
     res_pass.BackgroundTexture.value = "day_skymap"
     res_pass.AlternativeBackgroundTexture.value = "night_skymap"
 
-    sky_pass = avango.gua.nodes.SkyMapPassDescription(
-        OutputTextureName="day_skymap")
+    sky_pass = av.nodes.SkyMapPassDescription(OutputTextureName="day_skymap")
 
-    avango.gua.create_texture_cube(
+    av.create_texture_cube(
         "night_skymap",
         "data/textures/stars/purple-nebula/purple-nebula_right1.jpg",
         "data/textures/stars/purple-nebula/purple-nebula_left2.jpg",
@@ -203,26 +197,24 @@ def start():
         "data/textures/stars/purple-nebula/purple-nebula_front5.jpg",
         "data/textures/stars/purple-nebula/purple-nebula_back6.jpg")
 
-    pipeline_description = avango.gua.nodes.PipelineDescription(
-        Passes=[
-            sky_pass, avango.gua.nodes.TriMeshPassDescription(
-            ), avango.gua.nodes.LightVisibilityPassDescription(), res_pass,
-            avango.gua.nodes.TexturedScreenSpaceQuadPassDescription()
-        ])
+    pipeline_description = av.nodes.PipelineDescription(Passes=[
+        sky_pass, av.nodes.TriMeshPassDescription(
+        ), av.nodes.LightVisibilityPassDescription(
+        ), res_pass, av.nodes.TexturedScreenSpaceQuadPassDescription()
+    ])
 
     camera.PipelineDescription.value = pipeline_description
 
     graph.Root.value.Children.value.append(camera)
 
-    window = avango.gua.nodes.Window(
-        Size=size,
-        Title="shadows",
-        LeftResolution=size,
-        RightResolution=size,
-        EnableVsync=False,
-        StereoMode=avango.gua.StereoMode.MONO)
+    window = av.nodes.Window(Size=size,
+                             Title="shadows",
+                             LeftResolution=size,
+                             RightResolution=size,
+                             EnableVsync=False,
+                             StereoMode=av.StereoMode.MONO)
 
-    avango.gua.register_window("window", window)
+    av.register_window("window", window)
 
     navigator = examples_common.navigator.Navigator()
     navigator.StartLocation.value = camera.Transform.value.get_translate()
@@ -233,7 +225,7 @@ def start():
 
     camera.Transform.connect_from(navigator.OutTransform)
 
-    viewer = avango.gua.nodes.Viewer()
+    viewer = av.nodes.Viewer()
     viewer.SceneGraphs.value = [graph]
     viewer.Windows.value = [window]
 

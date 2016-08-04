bd4f6b919bfc3f93422e081893bfeaf234d91455
Jakob Wagner <wobakjerna@web.de>
2016-08-04 18:00:31 +0200

    implement TriMesh rendering-related fileds in skelanim, distribution of materials probably incomplete

--- avango-skelanim/python/SkeletalAnimationNode.cpp
+++ avango-skelanim/python/SkeletalAnimationNode.cpp
@@ -14,7 +14,7 @@ namespace boost
   {
     template <class T> struct pointee<av::Link<T> >
     {
-      typedef T type;
+      using type = T;
     };
   }
 }
@@ -34,11 +34,11 @@ void init_SkeletalAnimationNode()
   register_field<av::gua::skelanim::SFSkeletalAnimationNode>("SFSkeletalAnimationNode");
   register_multifield<av::gua::skelanim::MFSkeletalAnimationNode>("MFSkeletalAnimationNode");
 
-  class_< av::gua::skelanim::SkeletalAnimationNode,
+  class_<av::gua::skelanim::SkeletalAnimationNode,
           av::Link<av::gua::skelanim::SkeletalAnimationNode>,
-          bases<av::gua::GeometryNode>, boost::noncopyable 
-    >("SkeletalAnimationNode", "docstring", no_init)
-    .def("get_duration", &getAnimDuration)
-    .def("load_animation", &loadAnimation)
+          bases<av::gua::GeometryNode>, 
+          boost::noncopyable>("SkeletalAnimationNode", "docstring", no_init)
+            .def("get_duration", &getAnimDuration)
+            .def("load_animation", &loadAnimation)
     ;
 }

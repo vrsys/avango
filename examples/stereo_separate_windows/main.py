5eeb0fd22b595ac231fa4ae56b41ae26c34c2c65
Andreas-Christoph Bernstein <andreas.bernstein@uni-weimar.de>
2017-02-17 14:19:21 +0100

    Add comments

--- examples/stereo_separate_windows/main.py
+++ examples/stereo_separate_windows/main.py
@@ -22,89 +22,95 @@ class TimedRotate(avango.script.Script):
 
 def start():
 
-  # setup scenegraph
-  graph = avango.gua.nodes.SceneGraph(Name = "scenegraph")
+    # setup scenegraph
+    graph = avango.gua.nodes.SceneGraph(Name = "scenegraph")
 
-  loader = avango.gua.nodes.TriMeshLoader()
-  monkey = loader.create_geometry_from_file("monkey", "data/objects/monkey.obj", avango.gua.LoaderFlags.DEFAULTS)
+    loader = avango.gua.nodes.TriMeshLoader()
+    monkey = loader.create_geometry_from_file("monkey", "data/objects/monkey.obj", avango.gua.LoaderFlags.DEFAULTS)
 
-  light = avango.gua.nodes.LightNode(
-    Type=avango.gua.LightType.POINT,
-    Name = "light",
-    Color = avango.gua.Color(1.0, 1.0, 1.0),
-    Brightness = 10
-  )
-  light.Transform.value = avango.gua.make_trans_mat(1, 1, 2) * avango.gua.make_scale_mat(15, 15, 15)
+    light = avango.gua.nodes.LightNode(
+      Type=avango.gua.LightType.POINT,
+      Name = "light",
+      Color = avango.gua.Color(1.0, 1.0, 1.0),
+      Brightness = 10
+    )
+    light.Transform.value = avango.gua.make_trans_mat(1, 1, 2) * avango.gua.make_scale_mat(15, 15, 15)
 
-  # setup viewing
-  width = 1024
-  height = 768
-  eye_size = avango.gua.Vec2ui(width, height)
-  window_size = avango.gua.Vec2ui(width, height)
-  left_pos = avango.gua.Vec2ui(0, 0)
-  right_pos = avango.gua.Vec2ui(0, 0)
+    # setup viewing
+    width = 3840 #1024
+    height = 2160 #768
+    eye_size = avango.gua.Vec2ui(width, height)
+    window_size = avango.gua.Vec2ui(width, height)
+    left_pos = avango.gua.Vec2ui(0, 0)
+    right_pos = avango.gua.Vec2ui(0, 0)
 
-  if STEREO_MODE == avango.gua.StereoMode.SIDE_BY_SIDE:
-    right_pos.x = width + 1
-    window_size.x *= 2
+    if STEREO_MODE == avango.gua.StereoMode.SIDE_BY_SIDE:
+        right_pos.x = width + 1
+        window_size.x *= 2
 
-  window_left = avango.gua.nodes.Window(Size = window_size,
-                                       LeftPosition = left_pos,
-                                       LeftResolution = eye_size,
-                                       RightPosition = right_pos,
-                                       RightResolution = eye_size,
-                                       StereoMode = STEREO_MODE)
+    window_left = avango.gua.nodes.Window(
+        Size = window_size,
+        Display = ":0.0",
+        #SwapGroup = 1,
+        #SwapBarrier = 1,
+        LeftPosition = left_pos,
+        LeftResolution = eye_size,
+        RightPosition = right_pos,
+        RightResolution = eye_size,
+        StereoMode = STEREO_MODE)
 
-  avango.gua.register_window("window_left", window_left)
+    avango.gua.register_window("window_left", window_left)
 
-  window_right = avango.gua.nodes.Window(Size = window_size,
-                                       LeftPosition = left_pos,
-                                       LeftResolution = eye_size,
-                                       RightPosition = right_pos,
-                                       RightResolution = eye_size,
-                                       StereoMode = STEREO_MODE)
+    window_right = avango.gua.nodes.Window(
+        Size = window_size,
+        Display = ":0.0",  # ":0.1",
+        #SwapGroup = 1,
+        #SwapBarrier = 1,
+        LeftPosition = left_pos,
+        LeftResolution = eye_size,
+        RightPosition = right_pos,
+        RightResolution = eye_size,
+        StereoMode = STEREO_MODE)
 
-  avango.gua.register_window("window_right", window_right)
+    avango.gua.register_window("window_right", window_right)
 
+    cam = avango.gua.nodes.CameraNode(
+        Name = "cam",
+        LeftScreenPath = "/screen",
+        RightScreenPath = "/screen",
+        SceneGraph = "scenegraph",
+        Resolution = eye_size,
+        EyeDistance = 0.06,
+        EnableStereo = True,
+        LeftOutputWindow = "window_left",
+        RightOutputWindow = "window_right",
+        Transform = avango.gua.make_trans_mat(0.0, 0.0, 0.5)
+        # NearClip =
+    )
 
+    screen = avango.gua.nodes.ScreenNode(Name = "screen", Width = 0.5, Height = 0.5 * 0.3 / 0.4)
+    screen.Transform.value = avango.gua.make_trans_mat(0.0, 0.0, 2.5)
+    screen.Children.value = [cam]
 
-  cam = avango.gua.nodes.CameraNode(
-    Name = "cam",
-    LeftScreenPath = "/screen",
-    RightScreenPath = "/screen",
-    SceneGraph = "scenegraph",
-    Resolution = eye_size,
-    EyeDistance = 0.06,
-    EnableStereo = True,
-    LeftOutputWindow = "window_left",
-    RightOutputWindow = "window_right",
-    Transform = avango.gua.make_trans_mat(0.0, 0.0, 0.5)
-    # NearClip =
-  )
+    graph.Root.value.Children.value = [monkey, light, screen]
 
-  screen = avango.gua.nodes.ScreenNode(Name = "screen", Width = 0.5, Height = 0.5 * 0.3 / 0.4)
-  screen.Transform.value = avango.gua.make_trans_mat(0.0, 0.0, 2.5)
-  screen.Children.value = [cam]
+    #setup viewer
+    viewer = avango.gua.nodes.Viewer()
+    viewer.SceneGraphs.value = [graph]
+    viewer.Windows.value = [window_left, window_right]
+    viewer.DesiredFPS.value = 500.0
 
-  graph.Root.value.Children.value = [monkey, light, screen]
+    monkey_updater = TimedRotate()
 
-  #setup viewer
-  viewer = avango.gua.nodes.Viewer()
-  viewer.SceneGraphs.value = [graph]
-  viewer.Windows.value = [window_left, window_right]
-  viewer.DesiredFPS.value = 500.0
+    timer = avango.nodes.TimeSensor()
+    monkey_updater.TimeIn.connect_from(timer.Time)
 
-  monkey_updater = TimedRotate()
+    monkey.Transform.connect_from(monkey_updater.MatrixOut)
 
-  timer = avango.nodes.TimeSensor()
-  monkey_updater.TimeIn.connect_from(timer.Time)
+    guaVE = GuaVE()
+    guaVE.start(locals(), globals())
 
-  monkey.Transform.connect_from(monkey_updater.MatrixOut)
-
-  guaVE = GuaVE()
-  guaVE.start(locals(), globals())
-
-  viewer.run()
+    viewer.run()
 
 
 if __name__ == '__main__':

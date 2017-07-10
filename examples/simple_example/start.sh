59c92e552769c6acbdf3038cd2ba224222c889c1
Andreas-Christoph Bernstein <andreas.bernstein@uni-weimar.de>
2017-07-10 12:32:42 +0200

    Undo changes to start.sh introduced in db2d3d3

--- examples/simple_example/start.sh
+++ examples/simple_example/start.sh
@@ -8,14 +8,14 @@ LOCAL_GUACAMOLE="$DIR/../../../guacamole"
 LOCAL_AVANGO="$DIR/../../../avango"
 
 # if not, this path will be used
-GUACAMOLE=/home/bernste4/src/github/guacamole
-AVANGO=/home/bernste4/src/github/avango
+GUACAMOLE=/opt/guacamole/master
+AVANGO=/opt/avango/master
 
 # third party libs
 export LD_LIBRARY_PATH=/opt/boost/current/lib:/opt/zmq/current/lib:/opt/Awesomium/lib:/opt/pbr/inst_cb/lib
 
 # schism
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/bernste4/src/schism/lib/linux_x86
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/schism/current/lib/linux_x86
 
 # avango
 export LD_LIBRARY_PATH="$LOCAL_AVANGO/lib":$AVANGO/lib:$LD_LIBRARY_PATH:/opt/pbr/inst_cb/lib:/opt/Awesomium/lib

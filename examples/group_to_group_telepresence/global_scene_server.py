a023d814935758d6f5c1f7dc8a3852fb8b4f2003
Adrian Kreskowski <adrian.kreskowski@uni-weimar.de>
2018-09-02 22:30:43 +0200

    prepare animation & intermediate scene

--- examples/group_to_group_telepresence/global_scene_server.py
+++ examples/group_to_group_telepresence/global_scene_server.py
@@ -51,8 +51,62 @@ class TimedRotate(avango.script.Script):
 
     @field_has_changed(TimeIn)
     def update(self):
-        self.MatrixOut.value = avango.gua.make_rot_mat(self.TimeIn.value * 2.0,
-                                                       0.0, 1.0, 0.0) * avango.gua.make_scale_mat(0.7)
+        self.MatrixOut.value = avango.gua.make_rot_mat(self.TimeIn.value * 30.0,
+                                                       0.0, 1.0, 0.0) * avango.gua.make_scale_mat(1.0)
+
+
+class TimedKeyframePathAnimation(avango.script.Script):
+    TimeIn = avango.SFFloat()
+    MatrixOut = avango.gua.SFMatrix4()
+
+    walking_speed_units_per_second = 1.666667
+
+    indexed_keyframe_positions = [(0, avango.gua.Vec3(0, 0, -3)), 
+                                  (2000, avango.gua.Vec3(0, -0.40, 5) ),
+                                  (4000, avango.gua.Vec3(0, -0.40, 5) ),
+                                  (6000, avango.gua.Vec3(0, -0.40, 1) ),
+                                  (8000, avango.gua.Vec3(-1.5, 0, 1) ),
+                                  (10000, avango.gua.Vec3(-1.5, 0, 1) )
+                                ]
+    
+
+    animation_length_in_ms = indexed_keyframe_positions[-1][0]
+
+
+    @field_has_changed(TimeIn)
+    def update(self):
+        current_time_point = int(self.TimeIn.value*1000) % self.animation_length_in_ms
+        print("Time:" + str(current_time_point) )
+
+        keyframe_tuple_x = 0
+        keyframe_tuple_x_plus_1 = 0
+
+        found_first_time_point = False
+
+        current_keyframe_id = 0
+        for ikfp in self.indexed_keyframe_positions:
+            if not found_first_time_point:
+                if current_time_point == ikfp[0]:
+                    found_first_time_point = True
+                    keyframe_tuple_x = keyframe_tuple_x_plus_1 = ikfp
+                elif current_time_point > ikfp[0]:
+                    if (len(self.indexed_keyframe_positions) == current_keyframe_id + 1) or (self.indexed_keyframe_positions[current_keyframe_id + 1][0] > current_time_point) :
+                        found_first_time_point = True
+                        keyframe_tuple_x = ikfp
+            else:
+                if current_time_point < ikfp[0]:
+                    keyframe_tuple_x_plus_1 = ikfp
+                    break
+            current_keyframe_id = current_keyframe_id + 1
+
+        interpolation_weight = ((current_time_point) - keyframe_tuple_x[0]) / ((keyframe_tuple_x_plus_1[0]) - keyframe_tuple_x[0])
+        print( str(keyframe_tuple_x[0]) + "; " + str(current_time_point) + "; " + str(keyframe_tuple_x_plus_1[0]) )
+        print("interpolation_weight" + str(interpolation_weight))
+
+        interpolated_position = keyframe_tuple_x[1] * (1.0 - interpolation_weight) + keyframe_tuple_x_plus_1[1] * (interpolation_weight)
+
+        curr_pos = interpolated_position
+        self.MatrixOut.value = avango.gua.make_trans_mat(curr_pos[0], curr_pos[1], curr_pos[2])
 
 
 def make_node_distributable(node):
@@ -69,34 +123,17 @@ graph = avango.gua.nodes.SceneGraph(Name="scenegraph")
 loader = avango.gua.nodes.TriMeshLoader()
 
 
-#teapot = loader.create_geometry_from_file(
-#    "teapot", "./data/objects/teapot.obj")
+kaisersaal = loader.create_geometry_from_file("kaisersaal", "/mnt/data_internal/geometry_data/confidential/Kaisersaal/Ktris_7500/Bam_Kai_o_L_12_ct_0750.obj", avango.gua.LoaderFlags.DEFAULTS | avango.gua.LoaderFlags.LOAD_MATERIALS)
+
+kaisersaal.Transform.value = avango.gua.make_trans_mat(0.0, -1.0, 2.0) * avango.gua.make_rot_mat(-90.0, 1.0, 0.0, 0.0)
 
 
-#kaisersaal = loader.create_geometry_from_file("kaisersaal", "/mnt/data_internal/geometry_data/confidential/Kaisersaal/Ktris_7500/Bam_Kai_o_L_12_ct_0750.obj", avango.gua.LoaderFlags.DEFAULTS | avango.gua.LoaderFlags.LOAD_MATERIALS)
+lion = loader.create_geometry_from_file("loewe", "/home/wabi7015/Desktop/250k_hq_texture_loewe_quickfix.obj", avango.gua.LoaderFlags.DEFAULTS | avango.gua.LoaderFlags.LOAD_MATERIALS)
+lion.Transform.value = avango.gua.make_trans_mat(-2.5, -1.0, -1.0) * avango.gua.make_rot_mat(-90.0, 0.0, 1.0, 0.0) * avango.gua.make_scale_mat(1.0, 1.0, 1.0) 
 
-#kaisersaal.Transform.value = avango.gua.make_trans_mat(0.0, -1.0, 0.0) * avango.gua.make_rot_mat(-90.0, 1.0, 0.0, 0.0)
+kaisersaal.Transform.value = avango.gua.make_trans_mat(0.0, -1.0, 2.0) * avango.gua.make_rot_mat(-90.0, 1.0, 0.0, 0.0) 
 
 
-#lion = loader.create_geometry_from_file("loewe", "/home/wabi7015/Desktop/300k_loewe.obj", avango.gua.LoaderFlags.DEFAULTS | avango.gua.LoaderFlags.LOAD_MATERIALS)
-#lion.Transform.value = avango.gua.make_trans_mat(0.0, -1.0, 0.0) * avango.gua.make_rot_mat(-90.0, 0.0, 1.0, 0.0) * avango.gua.make_scale_mat(0.8, 0.8, 0.8) 
-
-#kaisersaal.Transform.value = avango.gua.make_trans_mat(0.0, -1.0, 0.0) * avango.gua.make_rot_mat(-90.0, 1.0, 0.0, 0.0) 
-#teapot.Transform.value = avango.gua.make_scale_mat(0.3, 0.3, 0.3)
-#teapot.Material.value.set_uniform("Color", avango.gua.Vec4(1.0, 0.766, 0.336,
-#                                                            1.0))
-#teapot.Material.value.set_uniform("Roughness", 0.3)
-#teapot.Material.value.set_uniform("Metalness", 1.0)
-
-"""
-teapot = plodloader.load_lod_pointcloud(
-                  "/mnt/pitoti/MA_MK/Intermediate_evaluation/test_files/loewe.bvh",
-                  avango.gua.lod.LoaderFlags.NORMALIZE_SCALE |
-                  avango.gua.lod.LoaderFlags.NORMALIZE_POSITION
-                  )
-
-teapot.Transform.value =  avango.gua.make_rot_mat(10, 0.0, 0.0, 1.0) * avango.gua.make_rot_mat(-45, 0.0, 1.0, 0.0) * avango.gua.make_rot_mat(90, 1.0, 0.0, 0.0) * avango.gua.make_trans_mat(-1.0, 0.0, 0.0) *  avango.gua.make_scale_mat(4.0, 4.0, 4.0) * teapot.Transform.value
-"""
 mat_desc = avango.gua.nodes.MaterialShaderDescription()
 mat_desc.load_from_file("data/materials/SimpleMaterial.gmd")
 avango.gua.register_material_shader(mat_desc, "mat")
@@ -109,28 +146,25 @@ nettrans.distribute_object(mat)
 spointsloader = avango.gua.nodes.SPointsLoader()
 spoints_geode = spointsloader.load("kinect", "/home/wabi7015/Programming/avango/examples/group_to_group_telepresence/spoints_resource_hekate_for_andromeda.sr")
 
-transform1 = avango.gua.nodes.TransformNode(Children=[spoints_geode])
-transform2 = avango.gua.nodes.TransformNode(
-    Transform=avango.gua.make_trans_mat(-0.5, 0.0, 0),
-    Children=[spoints_geode])
-
 light = avango.gua.nodes.LightNode(Type=avango.gua.LightType.POINT,
                                    Name="light",
                                    Color=avango.gua.Color(1.0, 1.0, 1.0),
-                                   Brightness=100.0,
+                                   Brightness=200.0,
                                    Transform=avango.gua.make_trans_mat(1, 1, 5)
                                    * avango.gua.make_scale_mat(30, 30, 30))
 
-monkey_transform1 = avango.gua.nodes.TransformNode(Name="monkey_transform1")
-monkey_transform1.Transform.value = avango.gua.make_trans_mat(1.0, 0.0, 0.0)
-#monkey_transform1.Children.value = [kaisersaal, lion]
+scene_transform = avango.gua.nodes.TransformNode(Name="scene_transform")
+scene_transform.Transform.value = avango.gua.make_trans_mat(1.0, 0.0, 0.0)
 
-monkey_transform2 = avango.gua.nodes.TransformNode(Name="monkey_transform2")
-monkey_transform2.Transform.value = avango.gua.make_trans_mat(-1.0, 0.0, 0.0)
-monkey_transform2.Children.value = [spoints_geode]
+#+++++++++++
+scene_transform.Children.value = [kaisersaal, lion]
+
+spoints_transform = avango.gua.nodes.TransformNode(Name="spoints_transform")
+spoints_transform.Transform.value = avango.gua.make_trans_mat(0.0, 0.0, 0.0)
+spoints_transform.Children.value = [spoints_geode]
 
 group = avango.gua.nodes.TransformNode(Name="group")
-group.Children.value = [monkey_transform2, light]
+group.Children.value = [spoints_transform, light]
 
 screen = avango.gua.nodes.ScreenNode(Name="screen", Width=4, Height=3)
 
@@ -211,27 +245,27 @@ occlusion_slave_client_cam = avango.gua.nodes.CameraNode(
     #needs to be invisible as soon as the real render-client comes into play
     )
 
-screen.Children.value = [occlusion_slave_client_cam, client_cam, server_cam, monkey_transform1]
+screen.Children.value = [occlusion_slave_client_cam, client_cam, server_cam, scene_transform]
 nettrans.Children.value = [group, screen]
 
 screen.Transform.value = avango.gua.make_trans_mat(0.0, 1.0, 2.5)
 
-navigator = examples_common.navigator.Navigator()
-navigator.StartLocation.value = screen.Transform.value.get_translate()
-navigator.OutTransform.connect_from(screen.Transform)
+#navigator = examples_common.navigator.Navigator()
+#navigator.StartLocation.value = screen.Transform.value.get_translate()
+#navigator.OutTransform.connect_from(screen.Transform)
 
-navigator.RotationSpeed.value = 0.12/30.0
-navigator.MotionSpeed.value = 0.07/10.0
+#navigator.RotationSpeed.value = 0.12/30.0
+#navigator.MotionSpeed.value = 0.07/10.0
 
 spoints_geode.Tags.value = ["invisible_osaka_avatar"]
 
 
-screen.Transform.connect_from(navigator.OutTransform)
+#screen.Transform.connect_from(navigator.OutTransform)
 
 graph.Root.value.Children.value = [nettrans]
 
 make_node_distributable(group)
-make_node_distributable(monkey_transform1)
+make_node_distributable(scene_transform)
 make_node_distributable(screen)
 
 nettrans.distribute_object(tri_pass)
@@ -264,12 +298,17 @@ viewer = avango.gua.nodes.Viewer()
 viewer.SceneGraphs.value = [graph]
 viewer.Windows.value = [window]
 
-monkey_updater = TimedRotate()
+
 
 timer = avango.nodes.TimeSensor()
-monkey_updater.TimeIn.connect_from(timer.Time)
 
-#teapot.Transform.connect_from(monkey_updater.MatrixOut)
+spoints_path_animator = TimedKeyframePathAnimation()
+spoints_path_animator.TimeIn.connect_from(timer.Time)
+spoints_transform.Transform.connect_from(spoints_path_animator.MatrixOut)
+
+#spoints_updater = TimedRotate()
+#spoints_updater.TimeIn.connect_from(timer.Time)
+#spoints_geode.Transform.connect_from(spoints_updater.MatrixOut)
 
 guaVE = GuaVE()
 guaVE.start(locals(), globals())

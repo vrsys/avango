dfd5099681208aa345f3d29b09f507b517089274
EphTron <es.90@gmx.de>
2019-04-25 19:44:01 +0200

    perspective picker has projection and texture mode

--- examples/efficient_exploration/src/PerspectivePicker.py
+++ examples/efficient_exploration/src/PerspectivePicker.py
@@ -33,7 +33,9 @@ class PerspectivePicker(avango.script.Script):
     self.offset = avango.gua.make_identity_mat()
     self.relevant_perspectives = []
     self.perspective_offsets = []
+    self.texture_coordinates = []
     self.visualizer = None
+    self.mode = 'texture' # 'projector'
 
 
   def set_projection_lense(self, obj, parent_node):
@@ -45,6 +47,7 @@ class PerspectivePicker(avango.script.Script):
     self.position_list = [li.position for li in self.localized_image_list]
     
   def find_closest_perspective(self):
+    print('find')
     closest_id = None
 
     # get direction vector of hendheld lense
@@ -77,6 +80,7 @@ class PerspectivePicker(avango.script.Script):
     angle_list.sort(key=lambda tup: tup[1])
     self.relevant_perspectives = []
     self.perspective_offsets = []
+    self.texture_coordinates = []
 
     for tup in angle_list:
       
@@ -84,16 +88,73 @@ class PerspectivePicker(avango.script.Script):
         # TODO: Might be good ideas:
         # - adapt frustum size based on distance of hendheld lense 
         # - check if the corners of the quad are in the frustum
+        _img = tup[0]
+
+        # step 1: get vector between camera and lense
+        # lense_pos = lense_trans.WorldTransform.value.get_translate()
+        line_p = _img.position
+        line_d = lense_pos - line_p
+        print(lense_pos)
+        line_d.normalize()
+
+        # step 2: direction vector of the camera
+        plane_p = _img.screen_transform.get_translate()
+        _rot_mat = avango.gua.make_rot_mat(_img.transform.get_rotate_scale_corrected())
+        plane_n = _rot_mat * avango.gua.Vec3(0.0,0.0,-1.0)
+        plane_n = avango.gua.Vec3(plane_n.x,plane_n.y,plane_n.z) # cast to vec3
+
+        pnpp_dot = plane_n.x*plane_p.x + plane_n.y*plane_p.y + plane_n.z*plane_p.z
+        # print('pnpp_dot',pnpp_dot)
+        # pnpp_dot = 1 if pnpp_dot >= 1 else pnpp_dot
+        # pnpp_dot = -1 if pnpp_dot <= -1 else pnpp_dot
+
+        pnlp_dot = plane_n.x*line_p.x + plane_n.y*line_p.y + plane_n.z*line_p.z
+        # print('pnlp_dot',pnlp_dot)
+        # pnlp_dot = 1 if pnlp_dot >= 1 else pnlp_dot
+        # pnlp_dot = -1 if pnlp_dot <= -1 else pnlp_dot
+
+        pnld_dot = plane_n.x*line_d.x + plane_n.y*line_d.y + plane_n.z*line_d.z
+        # print('pnld_dot',pnld_dot)
+        # pnld_dot = 1 if pnld_dot >= 1 else pnld_dot
+        # pnld_dot = -1 if pnld_dot <= -1 else pnld_dot
+
+        t = (pnpp_dot - pnlp_dot) / pnld_dot;
+        print(t)
+
+        # print('scalar', t)
+        intersection_pos =  line_p + (line_d * t);
+        print('inters at', intersection_pos)
+
+        x_values = [plane_p.x - _img.img_w_half, plane_p.x + _img.img_w_half]
+        y_values = [plane_p.y - _img.img_h_half, plane_p.y + _img.img_h_half]
+
+        # print(_img.img_h_half, y_values[1] - y_values[0])
+        ratio = 4.07 / 2.30
+        print(ratio)
+
+        u_coord = map_from_to(intersection_pos.x, x_values[0], x_values[1], _img.min_uv.x, _img.max_uv.x)
+        v_coord = map_from_to(intersection_pos.y, y_values[0], y_values[1], _img.min_uv.y, _img.max_uv.y)
+        print(_img.id, 'ORG uv coords min max', _img.min_uv, _img.max_uv)
+        print(_img.id, 'U and V coord', u_coord, v_coord)
+        if u_coord > _img.min_uv.x and u_coord < _img.max_uv.x:
+          print('yes')
+        zoom_factor = 1.5
+        max_uv = avango.gua.Vec2(u_coord - (_img.tile_w / 2 / zoom_factor) , v_coord - (_img.tile_w / 2 / zoom_factor))
+        # min_uv = avango.gua.Vec2(u_coord - (_img.tile_w / 2 / zoom_factor) , v_coord - (_img.tile_h / 2 / zoom_factor))
+        min_uv = avango.gua.Vec2(u_coord + (_img.tile_w / 2 / zoom_factor) , v_coord + (_img.tile_w / 2 / zoom_factor))
+        # max_uv = avango.gua.Vec2(u_coord + (_img.tile_w / 2 / zoom_factor) , v_coord + (_img.tile_h / 2 / zoom_factor))
+        min_max_coords = [min_uv, max_uv]
+        # print(_img.id, 'min max U V COORDS', u_coord, v_coord)
+        # print(_img.id, 'min max U V COORDS', min_uv, max_uv)
 
         closest_id = tup[0].id
-        # 
         lense_mat = self.projection_lense.WorldTransform.value
         projector_mat = self.localized_image_list[closest_id].transform
         offset = avango.gua.make_inverse_mat(lense_mat) * projector_mat
 
         self.relevant_perspectives.append(closest_id)
         self.perspective_offsets.append(offset)
-        
+        self.texture_coordinates.append(min_max_coords)
       else:
         closest_id = None
     if closest_id != None:
@@ -113,10 +174,13 @@ class PerspectivePicker(avango.script.Script):
   @field_has_changed(Button0)
   def button0_changed(self):
     if self.Button0.value:
-      self.update_perspective()
+      print('update perspective')
+      if self.mode is 'texture':
+        self.update_textures()
+      if self.mode is 'projector':
+        self.update_perspective()
       print(self.relevant_perspectives[0:16])
       self.button0_pressed = True
-      # print('TASTE 0')
     else:
       self.button0_pressed = False
       
@@ -129,9 +193,28 @@ class PerspectivePicker(avango.script.Script):
       # print(self.perspective_offsets[0])
       self.visualizer.update_images(self.relevant_perspectives[0:self.visualizer.views],
                                     self.perspective_offsets[0:self.visualizer.views])
+    else:
+      print('No projective visualizer is set.')
 
-  def set_visualizer(self, visualizer):
+
+  def update_textures(self):
+    closest_id = self.find_closest_perspective()
+      
+    self.old_closest_id = closest_id
+    if self.visualizer:
+      self.visualizer.update_images(self.relevant_perspectives[0:self.visualizer.views], 
+                                    self.texture_coordinates[0:self.visualizer.views])
+    else:
+      print('No texture visualizer is set.')
+
+  def set_visualizer(self, visualizer, mode):
     self.visualizer = visualizer
+    if mode is 'texture' or mode is 'projector':
+      self.mode = mode
 
   def evaluate(self):
     pass
+
+def map_from_to(x,a,b,c,d):
+  y=(x-a)/(b-a)*(d-c)+c
+  return y

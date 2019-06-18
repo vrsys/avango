ddac870c2ee69ac9b46a57814221ff3498aa1214
Anton <anton.frolov@uni-weimar.de>
2019-06-18 11:22:59 +0200

    * final adjustments for deployment;

--- examples/vive_mlod_vt/start.bat
+++ examples/vive_mlod_vt/start.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set AVANGO_DIR=C:\Data\Repositories\vrsys_stack\avango
+set AVANGO_DIR=C:\dev\rep\avango
 set Path=%Path%;%AVANGO_DIR%\lib\Release
 set PYTHONPATH=%PYTHONPATH%;%AVANGO_DIR%\lib\python3.5;%AVANGO_DIR%\lib\python3.5\avango\daemon;%AVANGO_DIR%\examples
 
